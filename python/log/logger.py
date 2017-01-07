76bda806d91c521f9024f9023b7ef0d8dcb5f4df
hoaphumanoid <miguelgfierro@gmail.com>
2017-01-07 17:39:46 +0000

    logger

--- python/log/logger.py
+++ python/log/logger.py
@@ -5,23 +5,31 @@ import yaml
 
 
 def get_debug_level(debug_level):
-    if debug_level=='INFO':
+    """
+    Transform debug level from string to logging flags
+    :param debug_level: debug level as string
+    :return: debug level as logging flag
+    """
+    if debug_level == 'INFO':
         return logging.INFO
-    elif debug_level=='DEBUG':
+    elif debug_level == 'DEBUG':
         return logging.DEBUG
-    elif debug_level=='WARNING':
+    elif debug_level == 'WARNING':
         return logging.WARNING
-    elif debug_level=='ERROR':
+    elif debug_level == 'ERROR':
         return logging.ERROR
-    elif debug_level=='CRITICAL':
+    elif debug_level == 'CRITICAL':
         return logging.CRITICAL
     else:
         return None
 
 
-def setup_logger(path='', debug_level='ERROR'):
+def setup_logger(debug_level='ERROR', config_file=''):
     """
-    Setup logging configuration
+    Setup logging configuration.
+    :param debug_level: debug level as string
+    :param config_file: yaml configuration file
+    :return logging object
     """
 
     level = get_debug_level(debug_level)
@@ -33,61 +41,45 @@ def setup_logger(path='', debug_level='ERROR'):
         level = get_debug_level(env_level)
 
     # Get logger
-    logger = logging.getLogger()
-    logger.setLevel(level)
+    log = logging.getLogger()
+    log.setLevel(level)
 
     # Format logger
-    if os.path.exists(path):
-        with open(path, 'rt') as f:
+    if os.path.exists(config_file):
+        with open(config_file, 'rt') as f:
             config = yaml.safe_load(f.read())
         logging.config.dictConfig(config)
     else:
         console = logging.StreamHandler()
         format_str = '%(asctime)s -- %(filename)s:%(lineno)s -- %(levelname)s: %(message)s'
         console.setFormatter(logging.Formatter(format_str))
-        logger.addHandler(console)
+        log.addHandler(console)
 
-    return logger
+    return log
 
-
-
-def basic_logger():
-    """
-    Logger with default configuration
-    """
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
-    return logger
-
-
-def formatted_logger():
-    """
-    Logger with formatted output
-    """
-    logger = logging.getLogger(__name__)
-    console = logging.StreamHandler()
-    format_str = '%(asctime)s -- %(filename)s:%(lineno)s -- %(levelname)s: %(message)s'
-    console.setFormatter(logging.Formatter(format_str))
-    logger.addHandler(console)
-    logger.setLevel(logging.INFO)
-
-    return logger
-
-
-def log_something(logger):
-    logger.info("Info log")
-    logger.debug("Debug log")
-    logger.error("Error log")
-    logger.warning("Warning log")
 
 if __name__ == "__main__":
+    """
+    Showcase of 3 methods of setting up the logger: via console,
+    via yaml file and via environment variable
+    """
+    execute_example = 1
 
-    # Example no config file
-    print("Example of formatted logger")
-    logger_formatted = setup_logger(debug_level='DEBUG')
-    log_something(logger_formatted)
+    if execute_example == 1:
+        # Example no config file
+        print("Example of logger")
+        log = setup_logger(debug_level='DEBUG')
+        log.debug("Debug log")
+    elif execute_example == 2:
+        # Example config file
+        print("Example of logger configured via yaml file")
+        log = setup_logger(debug_level='INFO', config_file='logging.yaml')
+        log.info("Info log")
+        log.error("Error log")
+    elif execute_example == 3:
+        print("Example of logger with environment variable")
+        os.environ['DEBUG_LEVEL'] = "DEBUG"
+        log = setup_logger(debug_level='INFO')
+        log.debug("Debug log")
+        log.error("Error log")
 
-    # Example config file
-    print("Example of logger configured via yaml file")
-    logger_yaml = setup_logger(path='logging.yaml', debug_level='INFO')
-    log_something(logger_yaml)
