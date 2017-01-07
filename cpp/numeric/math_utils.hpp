d612f412810514d7b788b9431c2b15ccc8c1e086
hoaphumanoid <miguelgfierro@gmail.com>
2017-01-07 19:54:57 +0000

    fixed notation

--- cpp/numeric/math_utils.hpp
+++ cpp/numeric/math_utils.hpp
@@ -5,35 +5,35 @@
 #ifndef MATH_UTILS_HPP
 #define MATH_UTILS_HPP
 
-inline double rad_to_deg(double rad_angle) { return (rad_angle*180.0)/M_PI; }
-inline double deg_to_rad(double deg_angle) { return (deg_angle*M_PI)/180.0; }
+inline double radToDeg(double rad_angle) { return (rad_angle*180.0)/M_PI; }
+inline double degToRad(double deg_angle) { return (deg_angle*M_PI)/180.0; }
 
 template <class T, class U>
-inline bool in_range(T lower, U data, T upper){
+inline bool inRange(T lower, U data, T upper){
     return (data >= lower) && (data <= upper);
 }
 
-inline bool equal_float (float lhs, float rhs,
+inline bool equalFloat (float lhs, float rhs,
       float epsilon = std::numeric_limits<float>::epsilon()){
     return std::abs(lhs - rhs) <= epsilon;
 }
 
-inline bool less_or_equal_float (float lhs, float rhs,
+inline bool lessOrEqualFloat(float lhs, float rhs,
        float epsilon = std::numeric_limits<float>::epsilon()){
     return lhs <= (rhs+epsilon);
 }
 
-inline bool greater_or_equal_float (float lhs, float rhs,
+inline bool greaterOrEqualFloat(float lhs, float rhs,
        float epsilon = std::numeric_limits<float>::epsilon()){
     return (lhs+epsilon) >= rhs;
 }
 
-inline bool iseven(int num){
+inline bool isEven(int num){
     if (num % 2 == 0) return true;
     else return false;
 }
 
-inline bool isodd(int num){
+inline bool isOdd(int num){
     if (num % 2 == 0) return false;
     else return true;
 }
