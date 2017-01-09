0ea1c0bad1b93ad24a9bc0660e5825d8202174e7
hoaphumanoid <miguelgfierro@gmail.com>
2017-01-09 11:06:06 +0000

    added cmake list

---
+++ cpp/playground.cpp
@@ -0,0 +1,14 @@
+
+#include "io/read_file.hpp"
+#include "log/timer.hpp"
+#include "numeric/math_constants.hpp"
+#include "numeric/math_utils.hpp"
+
+int main(int argc, char *argv[]) {
+    std::cout << "Playground" << std::endl;
+
+
+    return 0;
+}
+
+
