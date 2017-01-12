b397155220411a1749c8a84bbe2c9e762aefa022
Miguel Fierro <miguelgfierro@gmail.com>
2017-01-11 21:54:09 -0500

    updated timer

--- cpp/log/timer.hpp
+++ cpp/log/timer.hpp
@@ -7,20 +7,19 @@
 class Timer
 {
 public:
-  void startTimer() { m_start = clock();};
-  void stopTimer() { m_end = clock();};
-  double getTimeInSec()
-  {
+  void startTimer() { m_start = clock();}
+  void stopTimer() { m_end = clock();}
+  double getTimeInSec(){
     m_elapsed_time_sec = (double)(m_end-m_start)/CLOCKS_PER_SEC;
     std::cout << "Time required for execution: " << m_elapsed_time_sec << " seconds" << std::endl;
     return m_elapsed_time_sec;
   }
-  double getTimeInMiliSec()
-  {
+  double getTimeInMiliSec(){
     m_elapsed_time_sec = (double)(m_end-m_start)/CLOCKS_PER_SEC;
     std::cout << "Time required for execution: " << m_elapsed_time_sec*1000 << " miliseconds" << std::endl;
     return m_elapsed_time_sec*1000;
   }
+
 private:
   clock_t m_start;
   clock_t m_end;
