a0f9ef37f7649362dac89557a24afc0bbc107685
Miguel González-Fierro <miguelgfierro@users.noreply.github.com>
2017-01-29 21:16:44 +0000

    Update system info python docstring

--- python/minsc/system_info.py
+++ python/minsc/system_info.py
@@ -1,18 +1,24 @@
 import sys
 import pkg_resources
 
-
+No
 def get_python_version():
+    """Get the system's python version.
+    Returns:
+        version (str): Python version.
+        
+    """
     return sys.version
 
 
 def get_library_version(library_name):
-    return pkg_resources.get_distribution(library_name).version
-
-
-if __name__ == "__main__":
-
-    print(get_python_version())
-
-    print(get_library_version("pandas"))
-
+    """Get the version of a library.
+    Parameters:
+        library_name (str): Name of the library.
+    Returns:
+        version (str): Version of the library.
+    Examples:
+        >>> print(get_library_version("pandas")
+       
+    """
+    return pkg_resources.get_distribution(library_name)
