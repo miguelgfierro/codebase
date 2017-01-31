16e9fac4b5f0c23b6167c38d609869bfafb4fcd3
Miguel Fierro <miguelgfierro@gmail.com>
2017-01-31 13:19:58 +0000

    matplot gray image

--- python/plot_base/matplot_base.py
+++ python/plot_base/matplot_base.py
@@ -3,13 +3,17 @@ import matplotlib.image as mpimg
 
 
 def plot_image(img):
-    """Plot an image
+    """Plot an image.
     Parameters:
         img (numpy array): An image.
     Examples:
         >>> img = mpimg.imread('../../share/Lenna.png')
         >>> plot_image(img)
+        >>> img_gray = mpimg.imread('../../share/Lenna_gray.png')
+        >>> plot_image(img_gray)
 
     """
-    plt.imshow(img)
+    cmap = None
+    if img.ndim == 2: cmap='gray'
+    plt.imshow(img, cmap=cmap)
     plt.show()
