f0d9115956af8c79eeb949af5466db20d57e8195
Miguel Fierro <miguelgfierro@gmail.com>
2017-02-03 22:33:20 +0000

    commented conversion categorical

--- python/pandas_base/conversion_categorical.py
+++ python/pandas_base/conversion_categorical.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
 
-def get_nominal_integer_dict(nominal_vals):
+def _get_nominal_integer_dict(nominal_vals):
+    """Convert nominal values in integers, starting at 0.
+    Parameters:
+        nominal_vals (pd.Series): A series.
+    Returns:
+        d (dict): An dictionary with numeric values.
+
+    """
     d = {}
     for val in nominal_vals:
         if val not in d:
@@ -10,25 +17,43 @@ def get_nominal_integer_dict(nominal_vals):
     return d
 
 
-def convert_to_integer(srs):
-    d = get_nominal_integer_dict(srs)
+def _convert_to_integer(srs):
+    """Convert series to integer.
+    Parameters:
+        srs (pd.Series): A series.
+    Returns:
+        srs (pd.Series): An series with numeric values.
+
+    """
+    d = _get_nominal_integer_dict(srs)
     return srs.map(lambda x: d[x])
 
 
 def convert_cols_categorical_to_numeric(df, col_list=None):
-    """
-    Convert categorical columns to numeric and leave numeric columns
+    """Convert categorical columns to numeric and leave numeric columns
     as they are. You can force to convert a numerical column if it is
     included in col_list
-    :param df: dataframe
-    :param col_list: optional list of columns
-    :return: dataframe with only numerical values
+    Parameters:
+        df (pd.DataFrame): Dataframe.
+        col_list (list): List of columns.
+    Returns:
+        ret (pd.DataFrame): An dataframe with numeric values.
+    Examples:
+        >>> df = pd.DataFrame({'letters':['a','b','c'],'numbers':[1,2,3]})
+        >>> df_numeric = convert_cols_categorical_to_numeric(df)
+        >>> print(df_numeric)
+           letters  numbers
+        0        0        1
+        1        1        2
+        2        2        3
+
     """
+    if col_list is None: col_list = []
     ret = pd.DataFrame()
     for column_name in df.columns:
         column = df[column_name]
         if column.dtype == 'object' or column_name in col_list:
-            ret[column_name] = convert_to_integer(column)
+            ret[column_name] = _convert_to_integer(column)
         else:
             ret[column_name] = column
     return ret
@@ -38,19 +63,14 @@ def convert_cols_numeric_to_categorical(df):
     """
     Convert numerical columns to categorical and leave numeric columns
     as they are
-    :param df: dataframe
-    :return: dataframe with only numerical values
+    Parameters:
+        df (pd.DataFrame): Dataframe.
+    Returns:
+        ret (pd.DataFrame): An dataframe with numeric values.
+
     """
     #TODO
     pass
 
-
-if __name__ == "__main__":
-
-    df = pd.DataFrame({'letters':['a','b','c'],
-                       'numbers':[1,2,3]})
-    print("Example of conversion from categorical to numeric")
-    df_numeric = convert_cols_categorical_to_numeric(df)
-    print(df_numeric)
 
 
