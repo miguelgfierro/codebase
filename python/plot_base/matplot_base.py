3ee63bf585de0ef0b9eea8cba211762c476015c0
Miguel Fierro <miguelgfierro@gmail.com>
2017-02-28 23:26:56 +0000

    added plot histogram

--- python/plot_base/matplot_base.py
+++ python/plot_base/matplot_base.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import numpy as np
 
 
 def plot_image(img):
@@ -8,7 +9,6 @@ def plot_image(img):
         img (numpy array): An image.
     Examples:
         >>> img = mpimg.imread('../../share/Lenna.png')
-        >>> import numpy as np
         >>> print(np.array(img.shape, dtype='int'))
         [512 512   3]
         >>> plot_image(img)
@@ -22,4 +22,21 @@ def plot_image(img):
     if img.ndim == 2: cmap='gray'
     plt.imshow(img, cmap=cmap)
     plt.axis('off')
+    plt.show()
+
+
+def plot_histogram(hist, bins):
+    """Plot an histogram.
+    Parameters:
+        hist (numpy array): Array with the histogram values.
+        bins (numpy array): Array of the histogram bins.
+    Examples:
+        >>> x = 10 + 5*np.random.randn(1000)
+        >>> hist, bins = np.histogram(x, bins=50)
+        >>> plot_histogram(hist, bins)
+
+    """
+    width = np.diff(bins)
+    center = (bins[:-1] + bins[1:]) / 2
+    plt.bar(center, hist, align='center', width=width)
     plt.show()
