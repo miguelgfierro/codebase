e019af8f4863aa03c0b5f1e513e5371c48dee0bb
Miguel Fierro <miguelgfierro@gmail.com>
2017-03-23 23:11:15 +0000

    sql server connector python

---
+++ python/database/sql_server/connector.py
@@ -0,0 +1,34 @@
+import pyodbc
+
+
+def connect_to_database(database,
+                        user,
+                        password,
+                        driver='ODBC Driver 13 for SQL Server',
+                        server='localhost',
+                        port=1433):
+    """
+    Connect to a SQL server database.
+    Parameters:
+        database (str): Database name.
+        user (str): User name.
+        password (str): Password.
+        driver (str): SQL driver. For windows: 'ODBC Driver 13 for SQL Server',
+        for linux: 'FreeTDS'.
+        server(str): Server name.
+        port (int): Port of the SQL server.
+    Returns:
+        conn (object): Connector object.
+
+    """
+    db_driver = 'DRIVER={' + driver + '}'
+    port_num = 'PORT=' + str(port)
+    server_name = 'SERVER=' + server
+    db_name = 'DATABASE=' + database
+    uid = 'UID=' + user
+    pwd = 'PWD=' + password
+    connection_string = ';'.join([db_driver, server_name, port_num, db_name, uid, pwd])
+    conn = pyodbc.connect(connection_string)
+    return conn
+
+
