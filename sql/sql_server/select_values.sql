da8f0c9ceb4faa7bcdf7136c60de4d65721c82fe
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-03-31 23:05:30 +0100

    select top

--- sql/sql_server/select_values.sql
+++ sql/sql_server/select_values.sql
@@ -2,3 +2,6 @@
 SELECT TOP(1) * FROM table_name
 WHERE column_name = ? ORDER BY time_stamp DESC;
 
+-- Select top N values of a specific column names
+SELECT TOP (5) column_name1, column_name2 FROM table_name;
+
