7f99ad7e561aec74a8d4d3e11307aedcb2da5e3b
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-03-31 23:20:49 +0100

    select inner join

--- sql/sql_server/select_values.sql
+++ sql/sql_server/select_values.sql
@@ -5,3 +5,13 @@ WHERE column_name = ? ORDER BY time_stamp DESC;
 -- Select top N values of a specific column names
 SELECT TOP (5) column_name1, column_name2 FROM table_name;
 
+-- Select all entries equal to a value in a column
+SELECT * FROM table_name WHERE column_name = ?
+
+-- Select values from different tables. Example: There are 2 tables, one has ProductID, ProductNameEnglish
+-- and Price. The other table has ProductID and ProductNameSpanish. To select the price and the spanish
+-- name:
+SELECT t1.Price, t2.ProductNameSpanish FROM table_name1 AS t1
+INNER JOIN table_name2 AS t2 ON t1.ProductID = t2.ProductID
+
+
