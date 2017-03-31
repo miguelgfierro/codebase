015b408afd12262561ed297358dfdd78ab27c849
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-04-01 00:13:18 +0100

    insert value python sql

---
+++ python/database/sql_server/insert_values.py
@@ -0,0 +1,24 @@
+import pyodbc
+import datetime
+
+
+def insert_row(table_name, cursor, connector, value1, value2):
+    """Insert a row of items in a table.
+    Parameters:
+        table_name (str): Table name.
+        cursor (object): pyobdc cursor.
+        connector (object): pyodbc connector.
+        value1 (str or int): Value to insert.
+        value2 (datetime.py): Value to insert.
+    Example:
+        >>> conn = pyodbc.connect(connection_string)
+        >>> cur = conn.cursor()
+        >>> insert_row(tab_name, cur, conn, "item1", datetime.datetime.now())
+
+    """
+    query = "INSERT INTO " + table_name + "( name, date ) VALUES (?,?)"
+    cursor.execute(query, value1, value2)
+    connector.commit()
+
+
+
