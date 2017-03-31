a2ff872928843868af99eba99943092c5c271edc
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-04-01 00:04:00 +0100

    create table python sql

---
+++ python/database/sql_server/create_table.py
@@ -0,0 +1,20 @@
+import pyodbc
+
+
+def create_table(table_name, cursor):
+    """Create a table and drop it if it exists.
+    Parameters:
+        table_name (str): Table name.
+        cursor (object): pyobdc cursor.
+    Example:
+        >>> conn = pyodbc.connect(connection_string)
+        >>> cur = conn.cursor()
+        >>> create_table(tab_name, cur)
+
+    """
+    query = "IF OBJECT_ID(\'" + table_name + "\') IS NOT NULL DROP TABLE " + table_name + " "
+    query += "CREATE TABLE " + table_name
+    query += " ( user_id VARCHAR(50) not null, num INT not null, array VARBINARY(MAX) not null )"
+    cursor.execute(query)
+    cursor.commit()
+
