2100e90dd7bbb645817ba0e90ef67a1c2d5e37b6
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-04-02 16:18:54 +0100

    drop python

--- python/pandas_base/clean.py
+++ python/pandas_base/clean.py
@@ -43,7 +43,7 @@ def drop_columns(df, cols):
     """Drop columns.
     Parameters:
         df (pd.DataFrame): Dataframe.
-        value (int or str): Value
+        cols (int, str or list): Column name, column index or list of column names.
     Returns:
         df_return (pd.DataFrame): Dataframe with the replaced value.
     Examples:
@@ -56,3 +56,40 @@ def drop_columns(df, cols):
 
     """
     return df.drop(cols, axis=1)
+
+
+def drop_rows(df, rows):
+    """Drop rows.
+    Parameters:
+        df (pd.DataFrame): Dataframe.
+        rows (int, str or list): Row name, row index or list of row names.
+    Returns:
+        df_return (pd.DataFrame): Dataframe with the replaced value.
+    Examples:
+        >>> df = pd.DataFrame({'letters':['a','b','c'], 'numbers':[1,2,3]})
+        >>> drop_rows(df, 0)
+          letters  numbers
+        1       b        2
+        2       c        3
+
+    """
+    return df.drop(rows, axis=0)
+
+
+def drop_duplicates(df):
+    """Drop duplicate rows and reindex.
+    Parameters:
+        df (pd.DataFrame): Dataframe.
+    Returns:
+        df_return (pd.DataFrame): Dataframe with the replaced value.
+    Examples:
+        >>> df = pd.DataFrame({'letters':['b','b','c'], 'numbers':[2,2,3]})
+        >>> drop_duplicates(df)
+          letters  numbers
+        0       b        2
+        1       c        3
+
+    """
+    return df.drop_duplicates().reset_index(drop=True)
+
+
