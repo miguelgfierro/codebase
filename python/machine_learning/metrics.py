5a75d4542164636c613b88235f966a5106518d5d
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-04-22 20:30:43 +0100

    metrics regression

--- python/machine_learning/metrics.py
+++ python/machine_learning/metrics.py
@@ -1,4 +1,5 @@
-from sklearn.metrics import confusion_matrix, accuracy_score, roc_auc_score, f1_score, log_loss, precision_score, recall_score
+from sklearn.metrics import (confusion_matrix, accuracy_score, roc_auc_score, f1_score, log_loss, precision_score,
+                             recall_score, mean_squared_error, mean_absolute_error, r2_score)
 
 
 def classification_metrics_binary(y_true, y_pred):
@@ -41,8 +42,8 @@ def classification_metrics_binary_prob(y_true, y_prob):
     - AUC: The Area Under the Curve represents the ability to discriminate between positive and negative classes. An
     area of 1 represent perfect scoring and an area of 0.5 means random guessing.
     - Log loss: Also called logistic regression loss or cross-entropy loss. It quantifies the performance by
-    penalising false classifications. Minimising the Log Loss is equivalent to maximising the accuracy but using
-    probability predictions. Log loss penalize heavily classifiers that are confident about incorrect classifications.
+    penalizing false classifications. Minimizing the Log Loss is equivalent to minimizing the squared error but using
+    probabilistic predictions. Log loss penalize heavily classifiers that are confident about incorrect classifications.
     Parameters:
         y_true (list or array): True labels.
         y_prob (list or array): Predicted labels (probability).
@@ -64,3 +65,34 @@ def classification_metrics_binary_prob(y_true, y_prob):
     return report
 
 
+def regression_metrics(y_true, y_pred):
+    """
+    Returns a report with different metrics for a regression problem.
+    - Mean Squared Error: MSE is a risk metric corresponding to the expected value of the squared (quadratic) error.
+    It has the disadvantage of heavily weighting outliers.
+    - Mean Absolute Error: MAE is a risk metric corresponding to the expected value of the absolute error or L1 loss.
+    Not as sensitive to outliers.
+    - R Square: R2 is statistical measure of how close the data are to the fitted regression line. It's best possible
+    score is 1.0 and it can be negative (because the model can be arbitrarily worse). A score of 0 means that the
+    variables are not linearly correlated.
+    Parameters:
+        y_true (list or array): True values.
+        y_pred (list or array): Predicted values.
+    Returns:
+        report (dict): Dictionary with metrics.
+    Examples:
+        >>> y_true = [5,1,0,7,1]
+        >>> y_pred = [6,0.7,0.4,10,20]
+        >>> regression_metrics(y_true, y_pred)
+        {'MSE': 74.25, 'MAE': 4.7400000000000002, 'R2': -9.0883152173913029}
+        >>> y_true = [5,1,0,7,1]
+        >>> y_pred = [6,0.7,0.4,10,2]
+        >>> regression_metrics(y_true, y_pred)
+        {'MSE': 2.25, 'MAE': 1.1400000000000001, 'R2': 0.69429347826086962}
+
+    """
+    mse = mean_squared_error(y_true, y_pred)
+    mae = mean_absolute_error(y_true, y_pred)
+    r2 = r2_score(y_true, y_pred)
+    report = {'MSE':mse, 'MAE':mae, 'R2': r2}
+    return report
