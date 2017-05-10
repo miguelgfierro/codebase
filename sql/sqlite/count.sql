192ecac919e3a3b292d906aa3ed937a25e195e6a
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-05-10 10:20:14 -0700

    count sqlite

---
+++ sql/sqlite/count.sql
@@ -0,0 +1,10 @@
+-- Count total number of rows in a table
+SELECT COUNT(*) FROM table_name;
+
+-- Count the number of non null records in a column
+SELECT COUNT(column_name) FROM table_name;
+
+-- Count the number of distinct records in a column
+SELECT COUNT(DISTINCT column_name) FROM table_name;
+
+
