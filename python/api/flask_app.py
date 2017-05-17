7576bf0669d3246bd9bf2f06a2a5d464bb271caa
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-05-17 22:44:09 +0100

    added static files

--- python/api/flask_app.py
+++ python/api/flask_app.py
@@ -1,14 +1,15 @@
 from flask import Flask
 
 
-#HTML code
+# HTML code
 STATUS_OK = 200
 NOT_FOUND = 404
 BAD_REQUEST = 400
 BAD_PARAM = 450
 
 
-#app
+# app
 app = Flask(__name__)
 
-
+# define static folder for css, img, js
+app.static_folder = 'static'
