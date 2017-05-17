0cb95cae3b83caddb0ba830eaadd0169bac0d7f9
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-05-17 23:27:40 +0100

    updated css and flask html

--- python/api/flask_html_advanced.py
+++ python/api/flask_html_advanced.py
@@ -4,9 +4,9 @@ from flask import render_template
 
 @app.route("/")
 def index():
-	num = 95
+    num = 95
     return render_template('hello.html', number=num)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="127.0.0.1", port=5000)
