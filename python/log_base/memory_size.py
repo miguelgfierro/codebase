88cc32392fb4d9ca4e7081ce9b97f819fd635624
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-06-09 18:09:49 +0100

    get ram memory

--- python/log_base/memory_size.py
+++ python/log_base/memory_size.py
@@ -1,4 +1,6 @@
+from __future__ import division
 import sys
+import psutil
 
 
 def get_object_size(obj, units='Mb'):
@@ -7,7 +9,7 @@ def get_object_size(obj, units='Mb'):
         obj (obj or str or array): Object.
         units (str): Units [bytes, Kb, Mb, Gb]
     Returns:
-        size (int): Size of the object.
+        size (float): Size of the object.
     Examples:
         >>> get_object_size(7, 'bytes')
         24
@@ -27,3 +29,26 @@ def get_object_size(obj, units='Mb'):
     else:
         raise AttributeError("Units not correct")
 
+
+def get_ram_memory(units='Mb'):
+    """Get the RAM memory of the current machine.
+    Parameters:
+        units (str): Units [bytes, Kb, Mb, Gb]
+    Returns:
+        size (float): Memory size.
+    Examples:
+        >>> get_ram_memory('Gb')
+        4.0
+
+    """
+    s_bytes = psutil.virtual_memory()[0]
+    if units == 'bytes':
+        return s_bytes
+    elif units == 'Kb':
+        return s_bytes/1024
+    elif units == 'Mb':
+        return s_bytes/1024/1024
+    elif units == 'Gb':
+        return s_bytes/1024/1024/1024
+    else:
+        raise AttributeError("Units not correct")
