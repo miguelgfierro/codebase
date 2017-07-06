a3040430a999a7cdc4ef366d776937531637cb18
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-07-06 20:58:37 +0100

    same sign fix #80

--- python/numpy_base/array_evaluation.py
+++ python/numpy_base/array_evaluation.py
@@ -1,6 +1,33 @@
 import numpy as np
 
 
+def has_same_sign(data):
+    """ Evaluate if the array has all elements with the same sign.
+    Parameters:
+        data (numpy array): An array.
+    Returns:
+        flag (bool): Boolean with the evaluation.
+    Examples:
+        >>> data = np.array([(1,2,3),(2,3,4)])
+        >>> has_same_sign(data)
+        True
+        >>> data = np.array([0,0])
+        >>> has_same_sign(data)
+        WARNING: All zeros
+        True
+        >>> data = np.array([(0,0),(1,2)])
+        >>> has_same_sign(data)
+        False
+
+    """
+    try:
+        idx = next((idx for idx, val in np.ndenumerate(data) if val != 0))
+    except StopIteration:
+        print('WARNING: All zeros')
+        return True
+    return np.all(data > 0) if data[idx] > 0 else np.all(data < 0)
+
+
 def has_same_sign_or_zero(data):
     """ Evaluate if the array has all elements with the same sign or zero.
     Parameters:
