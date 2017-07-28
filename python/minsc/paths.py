a0f236e5361b865215849615d259ef022b7d16fc
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-07-28 21:03:01 +0100

    count filenames python fix #90

--- python/minsc/paths.py
+++ python/minsc/paths.py
@@ -1,10 +1,11 @@
 import os
+import glob
 
 
 def get_current_folder_path():
     """ Get parent path of current file.
     Returns:
-        path (string): parent path
+        path (str): parent path
     Examples:
         >>> print(get_current_folder_path())
         C:\\run3x\\codebase\\python\\minsc
@@ -16,7 +17,7 @@ def get_current_folder_path():
 def get_parent_folder_path():
     """ Get parent path of current file.
     Returns:
-        path (string): parent path
+        path (str): parent path
     Examples:
         >>> print(get_parent_folder_path())
         C:\\run3x\\codebase\\python
@@ -25,3 +26,15 @@ def get_parent_folder_path():
     return os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 
 
+def get_file_count(folderpath):
+    """ Return the number of files in a folder.
+    Parameters:
+        folderpath (str): folder path
+    Returns:
+        number (int): number of files in a folder
+    Examples:
+        >>> get_file_count('C:/run3x/codebase/python/minsc')
+        3
+
+    """
+    return len(glob.glob(os.path.join(folderpath,'*')))
