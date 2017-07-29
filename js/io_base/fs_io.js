ce2a2155710dd847476c7ec9ee50e489c42ed77f
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-07-29 18:21:55 +0100

    file io js fs fix #86

--- js/io_base/fs_io.js
+++ js/io_base/fs_io.js
@@ -2,6 +2,22 @@ var fs = require("fs");
 
 
 /**
+ * Save an array to a file (separated by commas)
+ * @param {Array} data - Array.
+ * @param {String} filename - Name of the file.
+ */
+function saveFile(data, filename){
+    var dataStr = '';
+    for(i=0;i<data.length;i++){
+        dataStr += data[i].toString() + '\n';
+    }
+    fs.writeFile(filename, dataStr, function(err, data){
+        if (err) console.log("ERROR: " + err);
+    });
+}
+
+
+/**
  * Read a csv file (separated by commas)
  * @param {String} filename - Name of the file.
  * @return {Array} - Array with the elements.
@@ -12,7 +28,9 @@ function readFile(filename){
     var lines = fileContents.toString().split('\n');
 
     for (var i = 0; i < lines.length; i++) {
-        items.push(lines[i].toString().split(','));
+        if (lines[i] != ''){
+            items.push(lines[i].toString().split(','));
+        }
     }
     return items;
 }
@@ -22,7 +40,23 @@ function readFile(filename){
  * Test with mocha. To run the test, in a terminal type:
  * mocha
  */
+
 var assert = require('assert');
+
+describe('test saveFile', function() {
+    it('save values and make sure they are correct', function() {
+        data = [
+        [1,2,3],
+        [4,5,6]
+        ];
+        saveFile(data, 'file.csv');
+        items = readFile('file.csv');
+        assert.deepEqual(['1','2','3'], items[0]);
+        assert.deepEqual(['4','5','6'], items[1]);
+    });
+});
+
+
 describe('test readFile', function() {
     it('should return the values in traj.csv', function() {
         items = readFile("../../share/traj.csv");
@@ -30,5 +64,7 @@ describe('test readFile', function() {
         assert.deepEqual(['0.0833333','444','206'], items[1]);
     });
 });
+
+
 
 
