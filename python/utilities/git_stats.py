5c16a6888e7c158ccd14ec09ed6c0412637901de
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-09-15 19:34:30 +0100

    added lines total, added, removed

--- python/utilities/git_stats.py
+++ python/utilities/git_stats.py
@@ -26,18 +26,18 @@ def get_current_watchers(url):
 def clone_repo(url):
     repo_dir = url.split('/')[-1]
     if os.path.isdir(repo_dir):
-        print("Repo {} already downloaded".format(repo_dir))
+        print('Repo {} already downloaded'.format(repo_dir))
         return repo_dir
     Repo.clone_from(url, repo_dir)
     if os.path.isdir(repo_dir):
         return repo_dir
     else:
-        raise Exception("Repo not downloaded correctly")
+        raise Exception('Repo not downloaded correctly')
 
 
 def get_number_commits(repo_dir):
     os.chdir(repo_dir)
-    resp = os.popen("git rev-list HEAD --count").read()
+    resp = os.popen('git rev-list HEAD --count').read()
     resp = int(resp.split('\n')[0])
     os.chdir('..')
     return resp
@@ -45,8 +45,44 @@ def get_number_commits(repo_dir):
 
 def get_number_branches(repo_dir):
     os.chdir(repo_dir)
-    resp = os.popen("git branch -a | wc -l").read()
+    resp = os.popen('git branch -a | wc -l').read()
     os.chdir('..')
     resp = int(resp.split('\n')[0])
     resp = resp - 1 #there is always one repeated origin/HEAD
+    return resp
+
+
+def get_number_tags(repo_dir):
+    os.chdir(repo_dir)
+    resp = os.popen('git tag | wc -l').read()
+    resp = int(resp.split('\n')[0])
+    return resp
+
+
+def get_number_contributors(repo_dir):
+    #FIXME: Not working I don' know why
+    os.chdir(repo_dir)
+    resp = os.popen('git shortlog -sne | wc -l').read()
+    resp = int(resp.split('\n')[0])
+    return resp
+
+
+def count_total_lines(repo_dir):
+    os.chdir(repo_dir)
+    resp = os.popen('git ls-files | xargs wc -l | grep total').read()
+    resp = int(resp.split(' total')[0])
+    return resp
+
+
+def count_added_lines(repo_dir):
+    os.chdir(repo_dir)
+    resp = os.popen('git log  --pretty=tformat: --numstat | awk \'{ add += $1 } END { printf "%s",add }\'').read()
+    resp = int(resp)
+    return resp
+
+
+def count_deleted_lines(repo_dir):
+    os.chdir(repo_dir)
+    resp = os.popen('git log  --pretty=tformat: --numstat | awk \'{ add += $1 ; subs += $2 } END { printf "%s",subs }\'').read()
+    resp = int(resp)
     return resp
