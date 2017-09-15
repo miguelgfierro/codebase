ad1dff340e014aaa03c8c128923f17ba22554f94
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-09-15 22:34:07 +0100

    git stats comments fix #98

--- python/utilities/git_stats.py
+++ python/utilities/git_stats.py
@@ -13,7 +13,7 @@ def get_current_forks(git_url, base_url='https://github.com/'):
         git_url (str): Github repo url.
         base_url (str): Base url of git repo.
     Returns:
-        resp (pint): Number of forks.
+        resp (int): Number of forks.
 
     """
     url = END_POINT + git_url.split(base_url)[1]
@@ -27,7 +27,7 @@ def get_open_issues(git_url, base_url='https://github.com/'):
         git_url (str): Github repo url.
         base_url (str): Base url of git repo.
     Returns:
-        resp (pint): Number of issues.
+        resp (int): Number of issues.
 
     """
     url = END_POINT + git_url.split(base_url)[1]
@@ -41,7 +41,7 @@ def get_current_stars(git_url, base_url='https://github.com/'):
         git_url (str): Github repo url.
         base_url (str): Base url of git repo.
     Returns:
-        resp (pint): Number of stars.
+        resp (int): Number of stars.
 
     """
     url = END_POINT + git_url.split(base_url)[1]
@@ -50,12 +50,46 @@ def get_current_stars(git_url, base_url='https://github.com/'):
 
 
 def get_current_watchers(git_url, base_url='https://github.com/'):
+    """Get current number of watchers.
+    Parameters:
+        git_url (str): Github repo url.
+        base_url (str): Base url of git repo.
+    Returns:
+        resp (int): Number of watchers.
+
+    """
     url = END_POINT + git_url.split(base_url)[1]
     resp = requests.get(url).json()
     return resp['subscribers_count']
 
 
+def last_year_commit_frequency(git_url, base_url='https://github.com/'):
+    """Get the commit frequency in every week of the last year.
+    Parameters:
+        git_url (str): Git repo url.
+        base_url (str): Base url of git repo.
+    Returns:
+        resp (dict): Dictionary of 52 elements (1 per week) with the commits
+                    every day (starting on Sunday), total commit sum and first
+                    day of the week.
+
+    """
+    url = END_POINT + git_url.split('https://github.com/')[1]
+    resp = requests.get(url + '/stats/commit_activity').json()
+    for id, item in enumerate(resp):
+        week_str = datetime.datetime.fromtimestamp(item['week']).strftime('%Y-%m-%d')
+        resp[id]['week'] = week_str
+    return resp
+
+
 def clone_repo(url):
+    """Clone a git repo.
+    Parameters:
+        url (str): Git repo url.
+    Returns:
+        repo_dir (str): Name of the folder name of the repo.
+
+    """
     repo_dir = url.split('/')[-1]
     if os.path.isdir(repo_dir):
         print('Repo {} already downloaded'.format(repo_dir))
@@ -68,6 +102,13 @@ def clone_repo(url):
 
 
 def get_number_commits(repo_dir):
+    """Get total number of commits.
+    Parameters:
+        repo_dir (str): Repo directory.
+    Returns:
+        resp (int): Number of commits.
+
+    """
     os.chdir(repo_dir)
     resp = os.popen('git rev-list HEAD --count').read()
     resp = int(resp.split('\n')[0])
@@ -76,6 +117,13 @@ def get_number_commits(repo_dir):
 
 
 def get_number_branches(repo_dir):
+    """Get total number of branches.
+    Parameters:
+        repo_dir (str): Repo directory.
+    Returns:
+        resp (int): Number of branches.
+
+    """
     os.chdir(repo_dir)
     resp = os.popen('git branch -a | wc -l').read()
     os.chdir('..')
@@ -85,6 +133,13 @@ def get_number_branches(repo_dir):
 
 
 def get_number_tags(repo_dir):
+    """Get total number of tags.
+    Parameters:
+        repo_dir (str): Repo directory.
+    Returns:
+        resp (int): Number of tags.
+
+    """
     os.chdir(repo_dir)
     resp = os.popen('git tag | wc -l').read()
     resp = int(resp.split('\n')[0])
@@ -92,6 +147,13 @@ def get_number_tags(repo_dir):
 
 
 def get_number_contributors(repo_dir):
+    """Get total number of contributors.
+    Parameters:
+        repo_dir (str): Repo directory.
+    Returns:
+        resp (int): Number of contributors.
+
+    """
     os.chdir(repo_dir)
     resp = os.popen('git log --format="%aN" | sort -u | wc -l').read()
     resp = int(resp.split('\n')[0])
@@ -99,6 +161,13 @@ def get_number_contributors(repo_dir):
 
 
 def count_total_lines(repo_dir):
+    """Get total number of lines.
+    Parameters:
+        repo_dir (str): Repo directory.
+    Returns:
+        resp (int): Number of lines.
+
+    """
     os.chdir(repo_dir)
     resp = os.popen('git ls-files | xargs wc -l | grep total').read()
     resp = int(resp.split(' total')[0])
@@ -106,6 +175,13 @@ def count_total_lines(repo_dir):
 
 
 def count_added_lines(repo_dir):
+    """Get the number of added lines.
+    Parameters:
+        repo_dir (str): Repo directory.
+    Returns:
+        resp (int): Number of added lines.
+
+    """
     os.chdir(repo_dir)
     resp = os.popen('git log  --pretty=tformat: --numstat | awk \'{ add += $1 } END { printf "%s",add }\'').read()
     resp = int(resp)
@@ -113,16 +189,15 @@ def count_added_lines(repo_dir):
 
 
 def count_deleted_lines(repo_dir):
+    """Get the number of deleted lines.
+    Parameters:
+        repo_dir (str): Repo directory.
+    Returns:
+        resp (int): Number of deleted lines.
+
+    """
     os.chdir(repo_dir)
     resp = os.popen('git log  --pretty=tformat: --numstat | awk \'{ add += $1 ; subs += $2 } END { printf "%s",subs }\'').read()
     resp = int(resp)
     return resp
 
-
-def monthly_commit_frequency(git_url):
-    url = END_POINT + git_url.split('https://github.com/')[1]
-    resp = requests.get(url + '/stats/commit_activity').json()
-    for id, item in enumerate(resp):
-        week_str = datetime.datetime.fromtimestamp(item['week']).strftime('%Y-%m-%d')
-        resp[id]['week'] = week_str
-    return resp
