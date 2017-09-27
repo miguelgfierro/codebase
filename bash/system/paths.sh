7c0ba5a161d16e779c0bc8ca0be4bf058c909218
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-09-27 18:51:58 +0100

    remove files except a certain number fix #103

--- bash/system/paths.sh
+++ bash/system/paths.sh
@@ -7,3 +7,6 @@ find . -type f -name '*.db' -delete
 #-n is line number, and
 #-w stands for match the whole word.
 grep -rnw '/path/to/somewhere/' -e 'text'
+
+#Remove files from a directory leaving a certain number, ex:100
+ls -1tr | head -n -100 | xargs -d '\n' rm -f --
