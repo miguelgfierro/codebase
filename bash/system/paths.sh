c92950a280c5bc438ab92966d9c73eb3ae1e47e5
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-09-27 18:47:20 +0100

    grep text fix #104

--- bash/system/paths.sh
+++ bash/system/paths.sh
@@ -1,2 +1,9 @@
 #Remove recursively all files with a pattern
 find . -type f -name '*.db' -delete
+
+
+#Find a text inside files in a directory.
+#-r or -R is recursive,
+#-n is line number, and
+#-w stands for match the whole word.
+grep -rnw '/path/to/somewhere/' -e 'text'
