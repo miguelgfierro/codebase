d2291611861233e07a89512f264401f7a2c3065d
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-09-27 20:06:45 +0100

    plot opencv image with matplotlib fix #105

--- python/plot_base/opencv_plot.py
+++ python/plot_base/opencv_plot.py
@@ -1,4 +1,5 @@
 import cv2
+import matplotlib.pyplot as plt
 
 
 def plot_image(img, title='image'):
@@ -17,3 +18,29 @@ def plot_image(img, title='image'):
     cv2.imshow(title,img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+def plot_image_matplotlib(img, figsize=(4,4)):
+    """Plot an opencv image using matplotlib.
+    Parameters:
+        img (numpy array): An image.
+        title (str): Title of the image.
+    Examples:
+        >>> img = cv2.imread('../../share/Lenna.png')
+        >>> plot_image_matplotlib(img)
+
+    """
+    shape_len = len(img.shape)
+    if shape_len == 3:#color image
+        image = img.copy()
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        cmap = None
+    elif shape_len == 2: #gray image
+        image = img
+        cmap = 'gray'
+    else:
+        raise Exception("Wrong image")
+    plt.figure(figsize = figsize)
+    plt.imshow(image, cmap=cmap)
+    plt.axis('off')
+    plt.show()
