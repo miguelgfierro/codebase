0563311a5b7b6d1a09e81d51add64b99f1ded7e3
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-09-30 13:27:00 +0100

    data structure stack fix #107

---
+++ python/data_structures/stack.py
@@ -0,0 +1,53 @@
+
+class Stack(object):
+    """A Stack is a linear structure of ordered items where  the addition
+    of new items and the removal of existing items always takes place at the same end.
+    This principle is called LIFO (Last In, First Out)
+    Examples:
+        >>> s = Stack()
+        >>> s.is_empty()
+        True
+        >>> s.push(4)
+        >>> s.push('dog')
+        >>> print(s)
+        ['dog', 4]
+        >>> s.peek()
+        'dog'
+        >>> s.size()
+        2
+        >>> s.pop()
+        'dog'
+        >>> s.size()
+        1
+
+    """
+    def __init__(self):
+        self.items = []
+
+    def __iter__(self):
+        """Return the stack as LIFO"""
+        return reversed(self.items)
+
+    def __str__(self):
+        """Return the string method of the stack as LIFO"""
+        return str(list(iter(self)))
+
+    def is_empty(self):
+        """See whether the stack is empty"""
+        return self.items == []
+
+    def push(self, item):
+        """Add a new item to the top of the stack"""
+        self.items.append(item)
+
+    def pop(self):
+        """Remove the top item from the stack"""
+        return self.items.pop()
+
+    def peek(self):
+        """Return the top item from the stack but does not remove it"""
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        """Return the number of items on the stack"""
+        return len(self.items)
