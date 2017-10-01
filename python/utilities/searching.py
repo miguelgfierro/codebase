26be20c4d24f08af0851e513a093844ef26a3f88
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-10-01 09:12:10 +0100

    ordered sequential search #111

--- python/utilities/searching.py
+++ python/utilities/searching.py
@@ -26,3 +26,40 @@ def sequential_search(alist, item):
         else:
             pos += 1
     return found
+
+
+def ordered_sequential_search(ordered_list, item):
+    """Ordered Sequential search
+    Complexity:
+    item is present: best case=1, worst case=n, avg=n/2
+    item not present: best case=1, worst case=n, avg=n/2
+    Parameters:
+        ordered_list (list): An ordered list.
+        item (int): The item to search.
+    Returns:
+        found (bool): Boolean with the answer of the search.
+    Examples:
+        >>> alist = [0, 1, 2, 8, 13, 17, 19, 32, 42]
+        >>> sequential_search(alist, 3)
+        False
+        >>> sequential_search(alist, 13)
+        True
+
+    """
+    pos = 0
+    found = False
+    stop = False
+    while pos < len(ordered_list) and not found and not stop:
+        if ordered_list[pos] == item:
+            found = True
+        else:
+            if ordered_list[pos] > item:
+                stop = True
+            else:
+                pos += 1
+    return found
+
+
+
+
+
