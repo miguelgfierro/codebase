d94dab3046d23959181c0e4b49b52316e15cd47b
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-10-01 09:07:36 +0100

    sequential search #111

---
+++ python/utilities/searching.py
@@ -0,0 +1,28 @@
+
+
+def sequential_search(alist, item):
+    """Sequential search
+    Complexity:
+    item is present: best case=1, worst case=n, avg=n/2
+    item not present: best case=n, worst case=n, avg=n
+    Parameters:
+        alist (list): A list.
+        item (int): The item to search.
+    Returns:
+        found (bool): Boolean with the answer of the search.
+    Examples:
+        >>> alist = [1, 2, 32, 8, 17, 19, 42, 13, 0]
+        >>> sequential_search(alist, 3)
+        False
+        >>> sequential_search(alist, 13)
+        True
+
+    """
+    pos = 0
+    found = False
+    while pos < len(alist) and not found:
+        if alist[pos] == item:
+            found = True
+        else:
+            pos += 1
+    return found
