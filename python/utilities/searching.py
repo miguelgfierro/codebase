3c51b85a4d8368ca25f34efeeccb15d4dce6833d
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-10-01 09:24:28 +0100

    binary search #111

--- python/utilities/searching.py
+++ python/utilities/searching.py
@@ -60,6 +60,36 @@ def ordered_sequential_search(ordered_list, item):
     return found
 
 
+def ordered_binary_search(ordered_list, item):
+    """Binary search in an ordered list
+    Complexity: O(log n)
+    Parameters:
+        ordered_list (list): An ordered list.
+        item (int): The item to search.
+    Returns:
+        found (bool): Boolean with the answer of the search.
+    Examples:
+        >>> alist = [0, 1, 2, 8, 13, 17, 19, 32, 42]
+        >>> sequential_search(alist, 3)
+        False
+        >>> sequential_search(alist, 13)
+        True
 
+    """
+    first = 0
+    last = len(ordered_list)-1
+    found = False
+
+    while first <= last and not found:
+        midpoint = (first + last)//2
+        if ordered_list[midpoint] == item:
+            found = True
+        else:
+            if item < ordered_list[midpoint]:
+                last = midpoint - 1
+            else:
+                first = midpoint + 1
+
+        return found
 
 
