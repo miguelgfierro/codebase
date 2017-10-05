990338bf4a00866e076ee3cb42ac8fee3d774208
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-10-05 20:00:08 +0100

    bash find files between dates fix #119

--- bash/system/paths.sh
+++ bash/system/paths.sh
@@ -1,12 +1,21 @@
-#Remove recursively all files with a pattern
-find . -type f -name '*.db' -delete
-
-
 #Find a text inside files in a directory.
 #-r or -R is recursive,
 #-n is line number, and
 #-w stands for match the whole word.
 grep -rnw '/path/to/somewhere/' -e 'text'
 
+
+#Find files modified between dates
+find . -newermt 'Oct 3 00:00' ! -newermt 'Oct 4 00:00' -ls
+
+
 #Remove files from a directory leaving a certain number, ex:100
 ls -1tr | head -n -100 | xargs -d '\n' rm -f --
+
+
+#Remove recursively all files with a pattern
+find . -type f -name '*.db' -delete
+
+
+#Remove files modified between dates
+find . -newermt 'Oct 3 00:00' ! -newermt 'Oct 4 00:00' -delete
