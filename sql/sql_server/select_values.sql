0b40f2282459f618ec2b565017e91f9b6205fb0e
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-10-06 08:02:36 +0100

    joins sql fix #118

--- sql/sql_server/select_values.sql
+++ sql/sql_server/select_values.sql
@@ -31,7 +31,7 @@ SELECT table1.column1, table2.column2...
 FROM table1
 LEFT JOIN table2
 ON table1.common_field = table2.common_field;
---Example:
+-- Example:
 SELECT TableA.firstName,TableA.lastName,TableB.age,TableB.Place
 FROM TableA
 LEFT JOIN TableB
@@ -43,3 +43,42 @@ arun                        prasanth                    24    kerala
 ann                         antony                      24    usa
 sruthy                      abc                         25    ekm
 new                         abc                         NULL  NULL
+
+
+-- RIGHT JOIN: will give all selected rows in TableB, plus any common selected rows in TableA.
+SELECT table1.column1, table2.column2...
+FROM table1
+RIGHT JOIN table2
+ON table1.common_field = table2.common_field;
+-- Example:
+SELECT TableA.firstName,TableA.lastName,TableB.age,TableB.Place
+FROM TableA
+RIGHT JOIN TableB
+ON TableA.id = TableB.id2;
+-- Result:
+firstName                   lastName                    age     Place
+...............................................................................
+arun                        prasanth                    24     kerala
+ann                         antony                      24     usa
+sruthy                      abc                         25     ekm
+NULL                        NULL                        24     chennai
+
+
+-- FULL JOIN: It is same as union operation, it will return all selected values from both tables.
+SELECT table1.column1, table2.column2...
+FROM table1
+FULL JOIN table2
+ON table1.common_field = table2.common_field;
+--Example:
+SELECT TableA.firstName,TableA.lastName,TableB.age,TableB.Place
+FROM TableA
+FULL JOIN TableB
+ON TableA.id = TableB.id2;
+-- Result:
+firstName                   lastName                    age    Place
+...............................................................................
+arun                        prasanth                    24    kerala
+ann                         antony                      24    usa
+sruthy                      abc                         25    ekm
+new                         abc                         NULL  NULL
+NULL                        NULL                        24    chennai
