2b9cee40377215a8e85a52609435e8b5fc9eb5b8
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-10-07 20:56:19 +0100

    search sort python

---
+++ python/data_structures/graph_search.py
@@ -0,0 +1,31 @@
+from graph import Graph, Vertex
+
+
+def breadth_first_search(graph, start, goal):
+    """Breadth-first search (BFS) is an algorithm for traversing or searching tree or graph data structures.
+    It starts at the tree root and explores the neighbor nodes first, before moving to the next level neighbours.
+    Parameters:
+        graph (Graph): A graph.
+        start (Vertex): The start vertex
+        goal (Vertex): The goal vertex
+    Examples:
+        >>> g = Graph()
+
+    """
+    pass
+
+
+def depth_first_search(graph, start, goal):
+    """Depth First Search is an algorithm for traversing or searching a graph. It starts at the root and explores
+     as far as possible along each branch before backtracking.
+    Parameters:
+        graph (Graph): A graph.
+        start (Vertex): The start vertex
+        goal (Vertex): The goal vertex
+    Examples:
+        >>> g = Graph()
+
+    """
+    pass
+
+
