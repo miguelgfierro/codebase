7731a350862b78dfe944f6df6283c783dd700f75
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-10-08 10:26:53 +0100

    dfs #123

--- python/data_structures/graph_search.py
+++ python/data_structures/graph_search.py
@@ -1,5 +1,6 @@
 from graph import Graph, Vertex
 from queue import Queue
+from stack import Stack
 
 
 def breadth_first_search(graph, start, goal):
@@ -30,7 +31,6 @@ def breadth_first_search(graph, start, goal):
         >>> [node.get_id() for node in path]
         ['1', '4', '7', '12']
 
-
     """
     v_start = graph.get_vertex(start)
     v_goal = graph.get_vertex(goal)
@@ -57,8 +57,38 @@ def depth_first_search(graph, start, goal):
         goal (Vertex): The goal vertex
     Examples:
         >>> g = Graph()
-
+        >>> for v in ['1', '2', '4', '7', '3']:
+        ...     _ = g.add_vertex(v)
+        >>> g.add_edge('1','2')
+        >>> g.add_edge('1','3')
+        >>> g.add_edge('1','4')
+        >>> g.add_edge('2','5')
+        >>> g.add_edge('2','6')
+        >>> g.add_edge('4','7')
+        >>> g.add_edge('4','8')
+        >>> g.add_edge('7','11')
+        >>> g.add_edge('7','12')
+        >>> g.add_edge('3','11')
+        >>> path = breadth_first_search(g, '1', '11')
+        >>> [node.get_id() for node in path]
+        ['1', '3', '11']
+        >>> path = breadth_first_search(g, '1', '12')
+        >>> [node.get_id() for node in path]
+        ['1', '4', '7', '12']
     """
-    pass
+    v_start = graph.get_vertex(start)
+    v_goal = graph.get_vertex(goal)
+    stack = Stack()
+    stack.push([v_start])
+    while not stack.is_empty():
+        path = stack.pop()
+        last_node = path[-1]
+        # path found
+        if last_node.get_id() == v_goal.get_id():
+            return path
+        for adjacent in last_node.get_connections():
+            new_path = list(path)
+            new_path.append(adjacent)
+            stack.push(new_path)
 
 
