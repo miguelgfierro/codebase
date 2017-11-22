8e11df21a50592b4d44b35a57d04c7b2b16b9f2e
Miguel Fierro <miguelgfierro@users.noreply.github.com>
2017-11-22 21:14:27 +0000

    draw feature importance #126

---
+++ python/machine_learning/feature_engineering.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+def _importance_dataframe(importances, features):
+    """Helper function to create a dataframe with the importance of each feature"""
+    importance_frame = pd.DataFrame({'Importance': list(importances), 'Feature': features})
+    importance_frame.sort_values(by='Importance', inplace=True)
+    return importance_frame
+
+
+def draw_feature_importance(importances, features, output_file='output.png'):
+    """Draw the feature importance as a bar chart in an output file
+    Parameters:
+        importances (array): Array with the importances.
+        features (list): List of the features which corresponds to `importances` array.
+        output_file (str): Filename of the output chart
+    """
+    importance_frame = _importance_dataframe(importances, features)
+    importance_frame.plot(kind='barh', x='Feature', figsize=(8,8), color='green')
+    plt.savefig(output_file)
+
+
+def plot_feature_importance(importances, features):
+    """Plot the feature importance as a bar chart
+    Parameters:
+        importances (array): Array with the importances.
+        features (list): List of the features which corresponds to `importances` array.
+    """
+    importance_frame = _importance_dataframe(importances, features)
+    importance_frame.plot(kind='barh', x='Feature', figsize=(8,8), color='green')
