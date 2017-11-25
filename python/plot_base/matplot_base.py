784d6929a73ea1d388cdd3d96801d0a6d9c19153
miguelgfierro <miguelgfierro@users.noreply.github.com>
2017-11-25 12:48:46 +0000

    plot trajectory interpolation fix #130

--- python/plot_base/matplot_base.py
+++ python/plot_base/matplot_base.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import interp1d
 
 
 def plot_image(img):
@@ -59,6 +60,32 @@ def plot_traj(y, x=None, title=None, xlabel=None, ylabel=None, color='b'):
     plt.figure()
     if x is None: x = range(len(y))
     plt.plot(x, y, color=color)
+    if title is not None: plt.title(title)
+    if xlabel is not None: plt.xlabel(xlabel)
+    if ylabel is not None: plt.ylabel(ylabel)
+    plt.show()
+
+
+def plot_traj_interpolate(y, x=None, title=None, xlabel=None, ylabel=None, color='b'):
+    """Plot a trajectory of points (x,y). If x is None it just take range(len(y)).
+    Parameters:
+        y (list or numpy array): Y axis values.
+        x (list or numpy array): X axis values.
+        title (str): Plot title.
+        xlabel (str): X axis label.
+        ylabel (str): Y axis label.
+        color (str): Matplotlib color https://matplotlib.org/api/colors_api.html?highlight=color#module-matplotlib.colors.
+    Examples:
+        >>> plot_traj([0.5, 0.7, 1.3, 1.7])
+        >>> plot_traj([0.5, 0.7, 1.3, 1.7], np.array([1,2,3,4]), title='Traj', xlabel='x', ylabel='y',color='r')
+
+    """
+    plt.figure()
+    if x is None: x = range(len(y))
+    dx = np.linspace(x[0], x[-1], num=len(y)*4, endpoint=True)
+    fy = interp1d(x, y, kind='cubic')
+    plt.plot(x, y, '.', color=color)
+    plt.plot(dx, fy(dx), color=color)
     if title is not None: plt.title(title)
     if xlabel is not None: plt.xlabel(xlabel)
     if ylabel is not None: plt.ylabel(ylabel)
