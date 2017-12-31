6d9e320c739aa4cfe425541fc7194ca74ba3e66d
miguelgfierro <miguelgfierro@users.noreply.github.com>
2017-12-31 11:10:53 +0100

    gist descriptor matlab #145

---
+++ matlab/image_base/gistdescriptor/demoGist.m
@@ -0,0 +1,31 @@
+% EXAMPLE 1
+% Load image
+img1 = imread('../../../share/Lenna.png');
+
+% Parameters:
+clear param
+%param.imageSize. If we do not specify the image size, the function LMgist
+%   will use the current image size. If we specify a size, the function will
+%   resize and crop the input to match the specified size. This is better when
+%   trying to compute image similarities.
+param.imageSize = [256 256]; % it works also with non-square images
+param.orientationsPerScale = [8 8 8 8];
+param.numberBlocks = 4;
+param.fc_prefilt = 4;
+
+% Computing gist requires 1) prefilter image, 2) filter image and collect
+% output energies
+[gist1, param] = LMgist(img1, '', param);
+
+% Visualization
+figure
+subplot(121)
+imshow(img1)
+title('Input image')
+subplot(122)
+showGist(gist1, param)
+title('Descriptor')
+
+
+
+
