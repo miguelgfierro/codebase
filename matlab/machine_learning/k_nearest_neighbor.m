98460e4a5451054406c57ba6449de4c6834f0915
miguelgfierro <miguelgfierro@users.noreply.github.com>
2017-12-31 13:53:00 +0100

    documentation fix

--- matlab/machine_learning/k_nearest_neighbor.m
+++ matlab/machine_learning/k_nearest_neighbor.m
@@ -1,6 +1,6 @@
 % K nearest neighbor
 %
-% knnsearch finds the nearest neighbor in X for each point in Y.
+% knnsearch finds the K nearest neighbor in X for each point in Y.
 % IDX is the index of the K nearest neighbors in X. 
 % D is the distance of the K nearest neighbor in X to Y.
 % IDX, D and Y have the same number of rows.
