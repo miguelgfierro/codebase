d9c8bede8952cf9af5f5ef07c33f8e6cca72d4a1
miguelgfierro <miguelgfierro@users.noreply.github.com>
2017-12-31 13:03:04 +0100

    knn matlab #147

---
+++ matlab/machine_learning/k_nearest_neighbor.m
@@ -0,0 +1,16 @@
+% K nearest neighbor
+%
+% knnsearch finds the nearest neighbor in X for each point in Y.
+% IDX is the index of the K nearest neighbors in X. 
+% D is the distance of the K nearest neighbor in X to Y.
+% IDX, D and Y have the same number of rows.
+% source: https://es.mathworks.com/help/stats/knnsearch.html
+
+rng('default') % for reproducibility
+X = randn(100000,512);
+Y = randn(2,512);
+tic
+[IDX,D] = knnsearch(X,Y,'K',5,'Distance','euclidean')
+toc
+
+
