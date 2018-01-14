0fe96640f1eaf91f81a0c1894e187b54e2fb7d69
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-01-14 20:30:32 +0000

    relu fix #150

--- python/machine_learning/activations.py
+++ python/machine_learning/activations.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 
-def softmax(vect):
+def softmax(x):
     """Softmax of a vector. It is the normalized exponential function. Softmax is a generalization of the logistic
     function that squashes a vector z into another vector  of real values in the range (0, 1) that add up to 1.
     Parameters:
-        vect (list or numpy array): Input list or array.
+        x (list or numpy array): Input list or array.
     Returns:
-        softmax (numpy array): The softmax of the array.
+        result (numpy array): The softmax of the array.
     Examples:
         >>> scores = [12, 5, 1]
         >>> softmax(scores)
@@ -16,5 +16,30 @@ def softmax(vect):
         1.0
 
     """
-    return np.exp(vect) / np.sum(np.exp(vect), axis=0)
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+
+def RELU(x, inplace=False):
+    """
+    Parameters:
+        x (list or numpy array): Input list or array.
+        inplace (bool): Inplace flag.
+    Returns:
+        result (numpy array): The RELU of the array (if inplace is True).
+    Examples:
+        >>> x = np.array([[1,1,-3],[0.5,-1,1]])
+        >>> RELU(x)
+        array([[ 1. ,  1. , -0. ],
+               [ 0.5, -0. ,  1. ]])
+        >>> RELU(x, inplace=True)
+        >>> x
+        array([[ 1. ,  1. ,  0. ],
+               [ 0.5,  0. ,  1. ]])
+
+    """
+    if inplace:
+        np.maximum(x, 0, x)
+    else:
+        return x * (x > 0)
+
 
