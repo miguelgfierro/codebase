aa725dcfd3387b80ce4060cb61164db4023891fe
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-03-25 14:21:43 +0200

    argparse example fix #174

--- python/io_base/argument_io.py
+++ python/io_base/argument_io.py
@@ -2,7 +2,21 @@ import argparse
 
 
 # FIXME: refactor with tests
-def parser_example():
+def parse_args(arguments):
+    """
+    Examples:
+        >>> import subprocess
+        >>> import sys
+        >>> cmd = 'AAA -ms BBB -si -l 1 2 3'
+        >>> args = parse_args(['AAA', '-ms', 'BBB', '-si', '-l', '1', '2', '3'])
+        Mandatory string: AAA
+        Mandatory string -ms: BBB
+        Store true -si: True
+        Input list ['1', '2', '3'], type: <class 'list'>
+        Default list [7, 77, 777], type: <class 'list'>
+
+    """
+
     parser = argparse.ArgumentParser(description="Parser",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('mandatory_string', type=str, help='Help mandatory string param') #here we can't put 'mandatory-string'
@@ -14,22 +28,13 @@ def parser_example():
     parser.add_argument('-no', '--no-me-gusta', action='store_false', help='Help false parameter')
     parser.add_argument('-l','--list', nargs='+', help='List of arguments, ex: python argument_io.py -l 1 2 3')
     parser.set_defaults(my_list=[7, 77, 777], my_string='bazinga')
-    args = parser.parse_args()
+    args = parser.parse_args(arguments)
     print("Mandatory string:", args.mandatory_string)
     print("Mandatory string -ms:", args.mand_str)
     print("Store true -si:", args.me_gusta)
     print("Input list {}, type: {}".format(args.list, type(args.list)))
     print("Default list {}, type: {}".format(args.my_list, type(args.my_list)))
-    """
-    $ python argument_io.py AAA -ms BBB -si -l 1 2 3
-    Mandatory string: AAA
-    Mandatory string -ms: BBB
-    Store true -si: True
-    Input list ['1', '2', '3'], type: <class 'list'>
-    Default list [7, 77, 777], type: <class 'list'>
-    """
+    return args
 
-if __name__ == '__main__':
-    parser_example()
 
 
