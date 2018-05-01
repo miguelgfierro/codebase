406a8fdee753206122ecc31927ecaceeaa162146
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-05-01 06:55:50 +0100

    plot function 2D #198

--- python/optimization/functions.py
+++ python/optimization/functions.py
@@ -1,5 +1,9 @@
 from scipy.optimize import rosen
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.pyplot as plot
 
 
 def rosenbrock(x):
@@ -17,3 +21,28 @@ def ackley(x):
     arg1 = -0.2 * np.sqrt(0.5 * (x[0] ** 2 + x[1] ** 2))
     arg2 = 0.5 * (np.cos(2. * np.pi * x[0]) + np.cos(2. * np.pi * x[1]))
     return -20. * np.exp(arg1) - np.exp(arg2) + 20. + np.e
+
+
+def plot_function2D(x, y, z):
+    """Plot 2D function with matplotlib
+    Args:
+        x (array): X value.
+        y (array): Y value.
+        z (array): function value.
+    Examples:
+        >>> s = 0.05
+        >>> X = np.arange(-2, 2.+s, s)
+        >>> Y = np.arange(-2, 6.+s, s)
+        >>> X, Y = np.meshgrid(X, Y)
+        >>> f = rosenbrock([X, Y])
+        >>> plot_function2D(X, Y, f)
+
+    """
+    fig = plot.figure()
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap=cm.coolwarm,
+             linewidth=0, antialiased=False)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plot.show()
