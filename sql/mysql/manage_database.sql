929ed458db80e4eb7cc7017c364c15de59e82579
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-06-18 03:10:41 +0100

    my sql database fix #211

---
+++ sql/mysql/manage_database.sql
@@ -0,0 +1,12 @@
+-- Create new database
+CREATE DATABASE my_database;
+
+-- Show available databases
+SHOW DATABASES;
+
+-- Select database
+USE my_database;
+
+-- Remove database
+DROP DATABASE my_database;
+
