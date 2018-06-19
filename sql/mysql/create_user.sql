94df78f100fa4d792fdfc18008d06275d56faff9
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-06-19 06:21:09 -0400

    authentication in sql fix #218

--- sql/mysql/create_user.sql
+++ sql/mysql/create_user.sql
@@ -1,7 +1,11 @@
 -- Create a new SQL login user
 -- Replace login name 'miguel' and password with your choice of user name/password
+-- In version 8, the default authentification method is caching sha2
+-- https://dev.mysql.com/doc/refman/8.0/en/caching-sha2-pluggable-authentication.html 
 CREATE USER IF NOT EXISTS 'miguel'@'localhost' IDENTIFIED BY 'miguel1234';
  
-
+-- To create a new user with the legacy password method:
+CREATE USER 'nativeuser'@'localhost'
+IDENTIFIED WITH mysql_native_password BY 'password';
 
 
