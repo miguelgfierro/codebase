671e0f17bbd4fb5210a7f9e21cbcaca04aec4ac8
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-06-19 07:00:40 -0400

    inserting files in mysql (work in progress)

--- sql/mysql/insert_values.sql
+++ sql/mysql/insert_values.sql
@@ -1,9 +1,9 @@
----
---- Example of values insertion in a table
----
+--
+-- Example of values insertion in a table
+--
 USE my_database;
 
---- The primary key is an auto-increment value, there is no need to insert it
+-- The primary key is an auto-increment value, there is no need to insert it
 INSERT INTO table_name 
 (
 recid,
@@ -23,3 +23,15 @@ VALUES
 
 SELECT * FROM table_name;
 
+-- Load a csv file
+LOAD DATA LOCAL INFILE '/Users/miguel/run3x/codebase/share/traj.csv'
+INTO TABLE table_csv  
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+(t, q0 , q1);
+
+LOAD DATA INFILE '/Users/miguel/run3x/codebase/share/traj.csv' 
+INTO TABLE table_csv;
+
+SELECT * FROM table_csv;
