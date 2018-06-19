0fee162ab230c4f2629256ebda71cdec97646a40
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-06-19 06:44:02 -0400

    small typo

--- sql/mysql/create_table.sql
+++ sql/mysql/create_table.sql
@@ -10,13 +10,13 @@ DROP TABLE IF EXISTS table_name;
 CREATE TABLE table_name 
 (
   id SMALLINT NOT NULL auto_increment,   
-  recid int(11) NOT NULL DEFAULT '0',       
+  recid INT(11) NOT NULL DEFAULT '0',       
   filename VARCHAR(250)  NOT NULL DEFAULT '',     
-  num int(11) NULL, 
+  num INT(11) NULL, 
   float_num FLOAT(8,6) NOT NULL DEFAULT '0',
   letter CHAR(1),
   input_date DATE,
-   PRIMARY KEY  (id)
+  PRIMARY KEY  (id)
 );
 
 DESCRIBE table_name;
