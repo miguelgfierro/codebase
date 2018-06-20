6708c7393afac51248d14f0129c0f13da6f09fc7
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-06-20 07:23:56 -0400

    insert values #222

---
+++ sql/sqlite/insert_values.sql
@@ -0,0 +1,21 @@
+-- Insert values in a table
+INSERT INTO table_name 
+(
+id,
+recid,
+filename,
+float_num,
+letter,
+input_date
+)
+VALUES 
+(
+0,
+77,
+'miguel.csv',
+3.14159,
+'A',
+'2017-04-12'
+);
+
+SELECT * FROM table_name;
