dee100f8300a9c52619b5c5db41d8518375e4279
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-06-23 07:00:10 -0400

    reordered variables

--- python/database/sql_server/insert_values.py
+++ python/database/sql_server/insert_values.py
@@ -2,18 +2,18 @@ import pyodbc
 import datetime
 
 
-def insert_row(table_name, cursor, connector, value1, value2):
+def insert_row(cursor, connector, table_name, value1, value2):
     """Insert a row of items in a table.
     Args:
-        table_name (str): Table name.
         cursor (object): pyobdc cursor.
         connector (object): pyodbc connector.
+        table_name (str): Table name.
         value1 (str or int): Value to insert.
         value2 (datetime.py): Value to insert.
     Example:
         >>> conn = pyodbc.connect(connection_string)
         >>> cur = conn.cursor()
-        >>> insert_row(tab_name, cur, conn, "item1", datetime.datetime.now())
+        >>> insert_row(cur, conn, tab_name, "item1", datetime.datetime.now())
 
     """
     query = "INSERT INTO " + table_name + "( name, date ) VALUES (?,?)"
