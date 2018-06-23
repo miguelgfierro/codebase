c6303f4df9ff5e46cd6bf306fd08ef2137b80ee0
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-06-23 07:03:28 -0400

    fixed test in create table sqlite

--- python/database/sqlite/create_table.py
+++ python/database/sqlite/create_table.py
@@ -8,10 +8,10 @@ def create_table(cursor, table_name, table_instruction):
         table_name (str): Table name.
         table_instruction (str): Table variable definition.
     Example:
-        >>> conn = sqlite3.connect(':memory:')
+        >>> conn = sqlite3.connect('temp.db')
         >>> cur = conn.cursor()
         >>> instruction = '''num INT(11) NULL, float_num FLOAT(8,6) NOT NULL DEFAULT "0",letter CHAR(1),input_date DATE'''
-        >>> create_table('', cur, "table_name", instruction)
+        >>> create_table(cur, "table_name", instruction)
         >>> c = cur.execute('PRAGMA table_info([table_name]);')
         >>> cur.fetchall()
         [(0, 'num', 'INT(11)', 0, None, 0), (1, 'float_num', 'FLOAT(8,6)', 1, '"0"', 0), (2, 'letter', 'CHAR(1)', 0, None, 0), (3, 'input_date', 'DATE', 0, None, 0)]
