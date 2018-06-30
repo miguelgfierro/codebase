b1b50c5d85a2a901e1550d4a2aa20415c0498504
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-06-30 07:46:35 +0100

    working with websockets

--- python/api/flask_websocket.py
+++ python/api/flask_websocket.py
@@ -4,10 +4,40 @@
 # server to send content to the client without being first requested by the
 # client, and allowing messages to be passed back and forth while keeping the
 # connection open.
-# source: https://en.wikipedia.org/wiki/WebSocket
+# +info: https://en.wikipedia.org/wiki/WebSocket
+# https://github.com/miguelgrinberg/Flask-SocketIO/tree/master/example
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+
+@app.route('/')
+def index():
+    return render_template('websocket.html')
+
+
+@socketio.on('my event', namespace='/test')
+def test_message(message):
+    emit('my response', {'data': message['data']})
+
+
+@socketio.on('my broadcast event', namespace='/test')
+def test_message(message):
+    emit('my response', {'data': message['data']}, broadcast=True)
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+
+
+if __name__ == '__main__':
+    socketio.run(app)
