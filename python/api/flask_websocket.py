935c87e2d686b4f9576a6aa8522cd238a0678c1f
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-07-01 09:14:14 +0100

    websockets work in progress

--- python/api/flask_websocket.py
+++ python/api/flask_websocket.py
@@ -25,16 +25,20 @@ def handle_client_connect_event(json):
     print('received json from client: {0}'.format(str(json)))
 
 
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
 # @socketio.on('message')
 # def handle_json_button(json):
 #     print('received json from client: {0}'.format(str(json)))
 #     return_json = {}
 #     return_json['stuff I heard you say'] = json
 #     send(return_json, json=True)
-@socketio.on('message')
-def handle_json_button(json):
-    # it will forward the json to all clients.
-    send(json, json=True)
+# @socketio.on('message')
+# def handle_json_button(json):
+#     # it will forward the json to all clients.
+#     send(json, json=True)
 
 
 # @socketio.on('alert_button')
@@ -42,11 +46,17 @@ def handle_json_button(json):
 #     # it will forward the json to all clients.
 #     print('Message from Javascript client: {0}'.format(json))
 #     emit('alert', 'I see you pushed the ALERT button.')
-@socketio.on('alert_button')
-def handle_alert_event(json):
-    # it will forward the json to all clients.
-    print('Message from client was {0}'.format(json))
-    emit('alert', 'Message from backend')
+# @socketio.on('alert_button')
+# def handle_alert_event(json):
+#     # it will forward the json to all clients.
+#     print('Message from client was {0}'.format(json))
+#     emit('alert', 'Message from backend')
+
+@socketio.on('my event')                          # Decorator to catch an event called "my event":
+def test_message(message):                        # test_message() is the event callback function.
+    emit('my response', {'data': 'got it!'})      # Trigger a new event called "my response" 
+                                                  # that can be caught by another callback later in the program.
+
 
 
 if __name__ == '__main__':
--- python/api/static/js/websocket.js
+++ python/api/static/js/websocket.js
@@ -4,19 +4,30 @@ socket.on('connect', function () {
     socket.emit('client_connected', { data: 'New client!' });
 });
 
-socket.on('message', function (data) {
-    console.log('message form backend ' + data);
-});
+// socket.on('message', function (data) {
+//     console.log('message form backend ' + data);
+// });
 
-socket.on('alert', function (data) {
-    alert('Alert Message!! ' + data);
-});
+// socket.on('alert', function (data) {
+//     alert('Alert Message!! ' + data);
+// });
 
-function json_button() {
-    socket.send('json_button', '{"message": "test"}');
-}
+// function json_button() {
+//     socket.send('json_button', '{"message": "test"}');
+// }
 
-function alert_button() {
-    socket.send('alert_button', 'Message from client!!')
-}
+// function alert_button() {
+//     socket.send('alert_button', 'Message from client!!')
+// }
+
+
+    // this is a callback that triggers when the "my response" event is emitted by the server.
+    socket.on('my response', function(msg) {
+        $('#log').append('<p>Received: ' + msg.data + '</p>');
+    });
+    //example of triggering an event on click of a form submit button
+    $('form#emit').submit(function(event) {
+        socket.emit('my event', {data: $('#emit_data').val()});
+        return false;
+    });
 
