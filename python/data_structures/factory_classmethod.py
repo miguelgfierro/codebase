dc0afe0358e3cb78f1bf102cb9fceae9aedbc8b4
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-07-06 18:32:14 +0100

    factory with classmethod fix #235

---
+++ python/data_structures/factory_classmethod.py
@@ -0,0 +1,29 @@
+# From https://realpython.com/instance-class-and-static-methods-demystified/
+
+
+class Pizza:
+    """Factory using classmethod decorator
+    Examples:
+        >>> pizza = Pizza.margherita()
+        >>> type(pizza)
+        <class 'factory_classmethod.Pizza'>
+        >>> print(pizza)
+        Pizza(['mozzarella', 'tomatoes'])
+        >>> pizza2 = Pizza.prosciutto()
+        >>> print(pizza2)
+        Pizza(['mozzarella', 'tomatoes', 'ham'])
+
+    """
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
+
+    def __repr__(self):
+        return 'Pizza({})'.format(self.ingredients)
+
+    @classmethod
+    def margherita(cls):
+        return cls(['mozzarella', 'tomatoes'])
+
+    @classmethod
+    def prosciutto(cls):
+        return cls(['mozzarella', 'tomatoes', 'ham'])
