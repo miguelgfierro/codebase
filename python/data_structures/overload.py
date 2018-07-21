da7b9dcac96b3c536f4ffda586344f91d6dee33f
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-07-21 21:19:11 +0100

    overloading optional arguments

--- python/data_structures/overload.py
+++ python/data_structures/overload.py
@@ -8,10 +8,35 @@
 from functools import singledispatch
 
 
-def fun_multiple_dispatch(arg, verbose=True):
-    """Generic way of overloading a function.
-    The standard way to do overloading is using multiple dispatch
-    with helper functions.
+def fun_optional_arguments(arg1=None, arg2=None, arg3=None, verbose=True):
+    """Example of function overload with optional arguments.
+    Examples:
+        >>> fun_optional_arguments(arg1="Hi!", verbose=True)
+        Let me just say, Hi!
+        >>> fun_optional_arguments(arg2=5, verbose=True)
+        Strength in numbers, eh? 5
+        >>> fun_optional_arguments(arg3=[1,2,3], verbose=True)
+        Enumerate this: [1, 2, 3]
+
+    """
+    if arg1 is not None:
+        if verbose:
+            print("Let me just say,", end=" ")
+        print(arg1)
+    if arg2 is not None:
+        if verbose:
+            print("Strength in numbers, eh?", end=" ")
+        print(arg2)
+    if arg3 is not None:
+        if verbose:
+            print("Enumerate this:", end=" ")
+        print(arg3)
+
+
+def fun_multiple_dispatch(arg, verbose=False):
+    """Example of function overloading using multiple dispatch,
+    it requires the use of helper functions.
+    More info: http://matthewrocklin.com/blog/work/2014/02/25/Multiple-Dispatch
     Examples:
         >>> fun_multiple_dispatch("Hi!", verbose=True)
         Let me just say, Hi!
@@ -35,21 +60,21 @@ def fun_multiple_dispatch(arg, verbose=True):
 
 
 def _fun_str(arg, verbose=False):
-    """Overload of fun_base for argument string"""
+    """Overload of fun_multiple_dispatch for argument string"""
     if verbose:
         print("Let me just say,", end=" ")
     print(arg)
 
 
 def _fun_int(arg, verbose=False):
-    """Overload of fun_base for argument int"""
+    """Overload of fun_multiple_dispatch for argument int"""
     if verbose:
         print("Strength in numbers, eh?", end=" ")
     print(arg)
 
 
 def _fun_list(arg, verbose=False):
-    """Overload of fun_base for argument list"""
+    """Overload of fun_multiple_dispatch for argument list"""
     if verbose:
         print("Enumerate this:", end=" ")
     print(arg)
@@ -57,7 +82,7 @@ def _fun_list(arg, verbose=False):
 
 @singledispatch
 def fun_single_dispatch(arg, verbose=False):
-    """Single Dispatch Generic Functions.
+    """Overloading using single dispatch generic functions.
     A generic function is composed of multiple functions implementing the same operation for different types.
     When the implementation is chosen based on the type of a single argument, this is known as single dispatch.
     source: https://www.python.org/dev/peps/pep-0443/
