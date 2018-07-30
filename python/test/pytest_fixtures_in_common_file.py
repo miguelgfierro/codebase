9f87c44949dcf90991c2af167b70bafcd32d9726
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-07-30 19:53:30 +0100

    small change in function name

--- python/test/pytest_fixtures_in_common_file.py
+++ python/test/pytest_fixtures_in_common_file.py
@@ -2,7 +2,7 @@
 # There is no need to import this file, pytest manage this by default.
 
 
-def test_unit_create_nn(nn5):
+def test_create_nn(nn5):
     assert nn5.name == "NN"
     assert nn5.n_layers == 5
     assert nn5.model is None
