5b544e50b14029def3c261f5444712b67aa9ed54
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-08-11 08:55:08 +0200

    small fixes in image utils

--- python/plot_base/matplot_base.py
+++ python/plot_base/matplot_base.py
@@ -20,7 +20,8 @@ def plot_image(img):
 
     """
     cmap = None
-    if img.ndim == 2: cmap='gray'
+    if img.ndim == 2:
+        cmap = 'gray'
     plt.imshow(img, cmap=cmap)
     plt.axis('off')
     plt.show()
@@ -44,7 +45,8 @@ def plot_histogram(hist, bins):
 
 
 def plot_traj(y, x=None, title=None, xlabel=None, ylabel=None, color='b'):
-    """Plot a trajectory of points (x,y). If x is None it just take range(len(y)).
+    """Plot a trajectory of points (x,y). If x is None it just take 
+    range(len(y)).
     Args:
         y (list or np.array): Y axis values.
         x (list or np.array): X axis values.
@@ -53,21 +55,28 @@ def plot_traj(y, x=None, title=None, xlabel=None, ylabel=None, color='b'):
         ylabel (str): Y axis label.
         color (str): Matplotlib color https://matplotlib.org/api/colors_api.html?highlight=color#module-matplotlib.colors.
     Examples:
-        >>> plot_traj([0.5, 0.7, 1.3, 1.7])
-        >>> plot_traj([0.5, 0.7, 1.3, 1.7], np.array([1,2,3,4]), title='Traj', xlabel='x', ylabel='y',color='r')
+        >>> input = [0.5, 0.7, 1.3, 1.7]
+        >>> plot_traj(input)
+        >>> plot_traj(input, np.array([1,2,3,4]), title='Traj', xlabel='x', ylabel='y',color='r')
 
     """
     plt.figure()
-    if x is None: x = range(len(y))
+    if x is None:
+        x = range(len(y))
     plt.plot(x, y, color=color)
-    if title is not None: plt.title(title)
-    if xlabel is not None: plt.xlabel(xlabel)
-    if ylabel is not None: plt.ylabel(ylabel)
+    if title is not None:
+        plt.title(title)
+    if xlabel is not None:
+        plt.xlabel(xlabel)
+    if ylabel is not None:
+        plt.ylabel(ylabel)
     plt.show()
 
 
-def plot_traj_interpolate(y, x=None, title=None, xlabel=None, ylabel=None, color='b'):
-    """Plot a trajectory of points (x,y). If x is None it just take range(len(y)).
+def plot_traj_interpolate(y, x=None, title=None, xlabel=None, ylabel=None,
+                          color='b'):
+    """Plot a trajectory of points (x,y). If x is None it just take 
+    range(len(y)).
     Args:
         y (list or np.array): Y axis values.
         x (list or np.array): X axis values.
@@ -76,17 +85,22 @@ def plot_traj_interpolate(y, x=None, title=None, xlabel=None, ylabel=None, color
         ylabel (str): Y axis label.
         color (str): Matplotlib color https://matplotlib.org/api/colors_api.html?highlight=color#module-matplotlib.colors.
     Examples:
-        >>> plot_traj([0.5, 0.7, 1.3, 1.7])
-        >>> plot_traj([0.5, 0.7, 1.3, 1.7], np.array([1,2,3,4]), title='Traj', xlabel='x', ylabel='y',color='r')
+        >>> input = [0.5, 0.7, 1.3, 1.7]
+        >>> plot_traj(input)
+        >>> plot_traj(input, np.array([1,2,3,4]), title='Traj', xlabel='x', ylabel='y',color='r')
 
     """
     plt.figure()
-    if x is None: x = range(len(y))
+    if x is None:
+        x = range(len(y))
     dx = np.linspace(x[0], x[-1], num=len(y)*4, endpoint=True)
     fy = interp1d(x, y, kind='cubic')
     plt.plot(x, y, '.', color=color)
     plt.plot(dx, fy(dx), color=color)
-    if title is not None: plt.title(title)
-    if xlabel is not None: plt.xlabel(xlabel)
-    if ylabel is not None: plt.ylabel(ylabel)
+    if title is not None:
+        plt.title(title)
+    if xlabel is not None:
+        plt.xlabel(xlabel)
+    if ylabel is not None:
+        plt.ylabel(ylabel)
     plt.show()
--- python/plot_base/opencv_plot.py
+++ python/plot_base/opencv_plot.py
@@ -15,32 +15,35 @@ def plot_image(img, title='image'):
         >>> plot_image(img)
 
     """
-    cv2.imshow(title,img)
+    cv2.imshow(title, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
-def plot_image_matplotlib(img, figsize=None):
+def plot_image_matplotlib(img, figsize=None, title=None):
     """Plot an opencv image using matplotlib.
     Args:
         img (np.array): An image.
         figsize (tuple): Size of the figure in inches (w,h).
+        title (str): Title of the image.
     Examples:
         >>> img = cv2.imread('../../share/Lenna.png')
         >>> plot_image_matplotlib(img)
 
     """
     shape_len = len(img.shape)
-    if shape_len == 3:#color image
+    if shape_len == 3:  # color image
         image = img.copy()
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         cmap = None
-    elif shape_len == 2: #gray image
+    elif shape_len == 2:  # gray image
         image = img
         cmap = 'gray'
     else:
         raise Exception("Wrong image")
     plt.figure(figsize=figsize)
+    if title is not None:
+        plt.title(title)
     plt.imshow(image, cmap=cmap)
     plt.axis('off')
     plt.show()
