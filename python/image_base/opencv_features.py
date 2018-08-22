c50237d3b445f9f23b9b4fbe39b87ef4e367ba8e
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-08-22 23:08:19 +0100

    contours fix #252

---
+++ python/image_base/opencv_features.py
@@ -0,0 +1,23 @@
+import cv2
+
+
+def largest_contour(mask):
+    """Get the largest contour from a binary image
+    Args:
+        mask (np.array): Binary image.
+    Returns:
+        largest_contour (np.array): Array of points.
+    Examples:
+        >>> mask = cv2.imread('../../share/Lenna_mask.png', 0)
+        >>> cnts = largest_contour(mask)
+        >>> len(cnts)
+        544
+
+    """
+    _, contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    largest_contour = contours[0]
+    return largest_contour
+
+
+
