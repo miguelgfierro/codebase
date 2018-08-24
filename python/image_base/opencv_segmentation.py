89bcb89bc0fd4ac980f9ea8b915f0bcec47da56e
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-08-24 19:30:35 +0100

    grabcut

--- python/image_base/opencv_segmentation.py
+++ python/image_base/opencv_segmentation.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 def apply_mask_to_image(img, mask):
@@ -42,3 +43,65 @@ def bounding_box(mask, max_contours=10):
         if y + h > y_max:
             y_max = y + h
     return x_min, y_min, x_max-x_min, y_max-y_min
+
+
+def grabcut_rect(img, rect, iterations=3):
+    """Grabcut segmentation using a rectangle as initial region of
+    confidence.
+    Args:
+        img (np.array): An image.
+        rect (tuple): A tuple of integers defining x, y, width and height.
+        iterations (int): Iterations.
+    Returns:
+        img_result (np.array): A segmented image.
+        mask (np.array): Binary image.
+    Examples:
+        >>> from codebase.python.plot_base.opencv_plot import plot_image_matplotlib as pp
+        >>> img = cv2.imread('../../share/Lenna.png')
+        >>> rect = (60, 32, 380, 480)
+        >>> img_result, mask = grabcut_rect(img, rect, iterations=1)
+        >>> pp(img_result)
+
+    """
+    img_result, mask = _grabcut(img, rect=rect, iterations=iterations)
+    return img_result, mask
+
+
+def grabcut_mask(img, mask, iterations=3):
+    """Grabcut segmentation using a mask as initial region of
+    confidence.
+    Args:
+        img (np.array): An image.
+        mask (np.array): Binary image.
+        iterations (int): Iterations.
+    Returns:
+        img_result (np.array): A segmented image.
+        mask (np.array): Binary image.
+    Examples:
+        >>> from codebase.python.plot_base.opencv_plot import plot_image_matplotlib as pp
+        >>> img = cv2.imread('../../share/Lenna.png')
+        >>> mask = cv2.imread('../../share/Lenna_mask.png', 0)
+        >>> img_result, mask = grabcut_mask(img, mask, iterations=1)
+        >>> pp(img_result)
+
+    """
+    img_result, mask = _grabcut(img, mask=mask, iterations=iterations)
+    return img_result, mask
+
+
+def _grabcut(img, mask=None, rect=None, iterations=3):
+    bgd_model = np.zeros((1, 65), np.float64)
+    fgd_model = np.zeros((1, 65), np.float64)
+    if mask is not None and rect is None:
+        method = cv2.GC_INIT_WITH_MASK
+        mask[mask == 255] = cv2.GC_FGD
+    elif rect is not None and mask is None:
+        method = cv2.GC_INIT_WITH_RECT
+    else:
+        raise ValueError("Error with the inputs mask or rect")
+    mask, _, _ = cv2.grabCut(img, mask, rect, bgd_model, fgd_model, iterations, method)
+    mask = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
+    img_result = img * mask[:, :, np.newaxis]
+    return img_result, mask
+
+
