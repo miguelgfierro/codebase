2a927fc073ad846f65e70c5ceb62693775123e80
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-10-07 15:46:16 +0200

    fixing pytest and unittest

--- python/test/conftest.py
+++ python/test/conftest.py
@@ -17,7 +17,7 @@ class NeuralNetwork:
     def _train_model(self, dataset):
         print("\nTraining neural network")
         sleep(1)
-        return 'model trained on ' + dataset
+        return "model trained on " + dataset
 
     def _compute_acc(self, model, dataset):
         return 0.42
@@ -28,24 +28,24 @@ def nn5():
     return NeuralNetwork(n_layers=5)
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def train_scope_function():
     nn = NeuralNetwork(2)
-    nn.train('dataset1')
+    nn.train("dataset1")
     return nn
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def train_scope_module():
     nn = NeuralNetwork(2)
-    nn.train('dataset1')
+    nn.train("dataset1")
     return nn
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def load_dataset():
-    filename = '../../share/traj.txt'
-    with open(filename, 'r') as f:
+    filename = "share/traj.txt"
+    with open(filename, "r") as f:
         data = [line.rstrip() for line in f.readlines()]
     yield data
     print("Tear down the fixture, with data: {}".format(data))
