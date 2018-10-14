d7ec58f136ddd6129a83cb6be1dec22d2761f4d5
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-10-13 20:34:51 -0400

    testing post, WIP

--- python/api/flask_basic.py
+++ python/api/flask_basic.py
@@ -97,7 +97,7 @@ class InvalidUsage(Exception):
 
     def to_dict(self):
         rv = dict(self.payload or ())
-        rv['message'] = self.message
+        rv["message"] = self.message
         return rv
 
 
--- python/api/flask_json.py
+++ python/api/flask_json.py
@@ -12,16 +12,51 @@ BAD_PARAM = 450
 app = Flask(__name__)
 
 
+@app.route("/api/v1/post_json", methods=["POST"])
+def post_status():
+    """Post method
+    Examples (not executable):
+    In bash Linux/Mac, equivalent to:
+    $ curl -X POST -d '{"param":"1"}' -H "Content-type: application/json" http://127.0.0.1:5000/api/v1/post_json
+    In Windows, equivalent to:
+    $ curl.exe -X POST -d '{\"param\":\"1\"}' -H "Content-type: application/json" http://127.0.0.1:5000/api/v1/post_json
+    In Python, equivalent to:
+    $ import requests, json
+    $ headers = {"Content-type":"application/json"}
+    $ data = {"param":"1"}
+    $ res = requests.post("http://127.0.0.1:5000/api/v1/post_json", data=json.dumps(data), headers=headers)
+    $ print(res.content)
+    {
+        "message": "Param = 1"
+    }
+    Examples:
+        >>> with app.test_client() as c:
+        ...     headers = {"Content-type":"application/json"}
+        ...     data = {"param":"1"}
+        ...     rv = c.post("/api/v1/post_json", data=data, headers=headers)
+        ...     status = rv.status
+        ...     content = rv.get_data()
+        >>> status
+        '200 OK'
+
+    """
+    if not request.json or "param" not in request.json:
+        abort(BAD_REQUEST)
+    param = request.json["param"]
+    resp = "Param = %s" % param
+    return make_response(jsonify({"message": resp}), STATUS_OK)
+
+
 @app.errorhandler(BAD_REQUEST)
 def bad_request(error):
     """Custom bad request response
     Examples (not executable):
         $ import requests
-        $ res = requests.post('http://127.0.0.1:5000/api/v1/post_json', data={"param":"2"})
+        $ res = requests.post("http://127.0.0.1:5000/api/v1/post_json", data={"param":"2"})
         $ res.ok
         False
         $ res.json()
-        {u'error': u'Bad request'}
+        {u"error": u"Bad request"}
 
     """
     return make_response(jsonify({"error": "Bad request"}), BAD_REQUEST)
@@ -32,38 +67,14 @@ def not_found(error):
     """Custom not found response
     Examples (not executable):
         $ import requests
-        $ res = requests.post('http://127.0.0.1:5000/api/v1/other', data={"other_param":"2"})
+        $ res = requests.post("http://127.0.0.1:5000/api/v1/other", data={"other_param":"2"})
         $ res.ok
         False
         $ res.json()
-        {u'error': u'Not found'}
+        {u"error": u"Not found"}
 
     """
     return make_response(jsonify({"error": "Not found"}), NOT_FOUND)
-
-
-@app.route("/api/v1/post_json", methods=["POST"])
-def post_status():
-    """Post method
-    Examples (executed in the command line in Linux/Mac):
-        $ curl -X POST -d '{"param":"1"}' -H "Content-type: application/json" http://127.0.0.1:5000/api/v1/post_json
-        In Windows:
-        $ curl.exe -X POST -d "{\"param\":\"1\"}" -H "Content-type: application/json" http://127.0.0.1:5000/api/v1/post_json
-        $ import requests, json
-        $ headers = {'Content-type':'application/json'}
-        $ data = {"param":"1"}
-        $ res = requests.post('http://127.0.0.1:5000/api/v1/post_json', data=json.dumps(data), headers=headers)
-        $ print(res.content)
-        {
-          "message": "Param = 1"
-        }
-
-    """
-    if not request.json or "param" not in request.json:
-        abort(BAD_REQUEST)
-    param = request.json["param"]
-    resp = "Param = %s" % param
-    return make_response(jsonify({"message": resp}), STATUS_OK)
 
 
 if __name__ == "__main__":
