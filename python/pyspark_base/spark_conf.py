7225af59729aebfbdd324045c17adf707e0c687f
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-11-11 09:34:27 +0100

    spark original conf

--- python/pyspark_base/spark_conf.py
+++ python/pyspark_base/spark_conf.py
@@ -6,7 +6,8 @@ def spark(app_name="App", url="local[*]", memory="10G", cores="16"):
     Args:
         app_name (str): Set name of the application
         url (str): URL for spark master.
-        memory (str): Size of memory for spark driver.  
+        memory (str): Size of memory for spark driver and executor.  
+        cores (str): Number of executor cores
     Returns:
         obj: Spark context.
     Examples: 
@@ -18,13 +19,13 @@ def spark(app_name="App", url="local[*]", memory="10G", cores="16"):
         SparkSession.builder.appName(app_name)
         .master(url)
         .config("spark.driver.memory", memory)
-        # .config("spark.executor.cores", cores)
-        # .config("spark.executor.memory", memory)
-        # .config("spark.memory.fraction", "0.9")
-        # .config("spark.memory.stageFraction", "0.3")
-        # .config("spark.executor.instances", 1)
-        # .config("spark.executor.heartbeatInterval", "36000s")
-        # .config("spark.network.timeout", "10000000s")
-        # .config("spark.driver.maxResultSize", "50g")
+        .config("spark.executor.cores", cores)
+        .config("spark.executor.memory", memory)
+        .config("spark.memory.fraction", "0.9")
+        .config("spark.memory.stageFraction", "0.3")
+        .config("spark.executor.instances", 1)
+        .config("spark.executor.heartbeatInterval", "36000s")
+        .config("spark.network.timeout", "10000000s")
+        .config("spark.driver.maxResultSize", "50g")
         .getOrCreate()
     )
