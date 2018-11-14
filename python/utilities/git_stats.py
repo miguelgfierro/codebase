ba039797a75a6b6c95b8e8297171a0c87074d219
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-11-14 08:14:47 +0100

    :bug:

--- python/utilities/git_stats.py
+++ python/utilities/git_stats.py
@@ -47,7 +47,7 @@ class Github:
         >>> g.repo_size >= 2
         True
         >>> g.creation_date
-        2016-12-27T13:23:55Z
+        '2016-12-27T13:23:55Z'
         >>> list(g.languages.keys())
         ['Dockerfile', 'JavaScript', 'SQL', 'HTML', 'CMake', 'Matlab', 'C++', 'Python', 'CSS', 'Jupyter Notebook']
         >>> g.number_languages >= 10
