694bbb3f89c6c66ff58c051899bb8c1b2cf13714
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-12-11 07:50:25 +0100

    email with html content

---
+++ php/email_base/email_with_html_content.php
@@ -0,0 +1,44 @@
+<?php
+/* Send an email with HTML content.
+
+To execute this, first make sure you have an apache server and php installed, 
+then execute:
+$ php my_file.php
+
+source: https://www.w3schools.com/php/func_mail_mail.asp
+NOTE: this will probably go to the spam folder.
+*/
+$to = "hoaphumanoid@gmail.com, somebodyelse@example.com";
+$subject = "HTML email";
+
+$message = "
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <webmaster@example.com>' . "\r\n";
+$headers .= 'Cc: myboss@example.com' . "\r\n";
+
+mail($to,$subject,$message,$headers);
+?> 
