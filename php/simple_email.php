753048c6cd2160146b73aa9ca913710c5395a182
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-12-11 07:39:57 +0100

    email with headers

--- php/simple_email.php
+++ php/simple_email.php
@@ -1,8 +1,12 @@
 <?php
 /* Send a simple email.
 
+To execute this, first make sure you have an apache server and php installed, 
+then execute:
+$ php my_file.php
+
 source: https://www.w3schools.com/php/func_mail_mail.asp
-NOTE: this will probably go to the spam folder, since it doesn't have headers
+NOTE: this will probably go to the spam folder.
 */
 
 // the message
