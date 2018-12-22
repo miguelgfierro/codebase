170caf02af0178ba54c549139a7bdb2569af5b85
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-12-22 10:34:03 +0100

    debuging in travis o_o

--- python/pyspark_base/spark_conf.py
+++ python/pyspark_base/spark_conf.py
@@ -15,18 +15,15 @@ def spark(app_name="App", url="local[*]", memory="10G", cores="16"):
         >>> spark is not None
         True
     """
-    return (
-        SparkSession.builder
-        # .appName(app_name)
-        # .master(url)
-        # .config("spark.driver.memory", memory)
-        # .config("spark.executor.cores", cores)
-        # .config("spark.executor.memory", memory)
-        # .config("spark.memory.fraction", "0.9")
-        # .config("spark.memory.stageFraction", "0.3")
-        # .config("spark.executor.instances", 1)
-        # .config("spark.executor.heartbeatInterval", "36000s")
-        # .config("spark.network.timeout", "10000000s")
-        # .config("spark.driver.maxResultSize", "50g")
-        .getOrCreate()
-    )
+    return SparkSession.builder.getOrCreate()
+    # .appName(app_name)
+    # .master(url)
+    # .config("spark.driver.memory", memory)
+    # .config("spark.executor.cores", cores)
+    # .config("spark.executor.memory", memory)
+    # .config("spark.memory.fraction", "0.9")
+    # .config("spark.memory.stageFraction", "0.3")
+    # .config("spark.executor.instances", 1)
+    # .config("spark.executor.heartbeatInterval", "36000s")
+    # .config("spark.network.timeout", "10000000s")
+    # .config("spark.driver.maxResultSize", "50g")
