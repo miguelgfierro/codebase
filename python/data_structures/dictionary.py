85b2cb2acf04230712f1d6a0247e74d3b8b6b059
miguelgfierro <miguelgfierro@users.noreply.github.com>
2019-02-15 19:26:30 +0000

    ordered dict

--- python/data_structures/dictionary.py
+++ python/data_structures/dictionary.py
@@ -7,11 +7,13 @@ def invert_dictionary(dictionary):
         dict: inverted dictionary
     Examples:
         >>> d = {1: 0, "a": 2, "b": "c"}
-        >>> invert_dictionary(d)
-        {0: 1, 2: 'a', 'c': 'b'}
+        >>> d_inv = invert_dictionary(d)
+        >>> OrderedDict(sorted(d_inv.items()))
+        OrderedDict([(0, 1), (2, 'a'), ('c', 'b')])
         >>> d = {1: 0, "a": 2, "b": "c", 5: 0}
-        >>> invert_dictionary(d)
-        {0: 5, 2: 'a', 'c': 'b'}
+        >>> d_inv = invert_dictionary(d)
+        >>> OrderedDict(sorted(d_inv.items()))
+        OrderedDict([(0, 5), (2, 'a'), ('c', 'b')])
 
     """
     return {v: k for k, v in dictionary.items()}
