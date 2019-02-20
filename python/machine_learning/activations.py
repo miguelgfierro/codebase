692b02a74839101b13590bc08fd8392d89bff8fe
miguelgfierro <miguelgfierro@users.noreply.github.com>
2019-02-20 22:36:29 +0000

    ml

--- python/machine_learning/activations.py
+++ python/machine_learning/activations.py
@@ -11,61 +11,70 @@ def softmax(x):
     """Softmax of a vector. It is the normalized exponential function. Softmax is a generalization of the logistic
     function that squashes a vector z into another vector of real values in the range (0, 1) that add up to 1.
     Used for multiclass classification.
+    
     Args:
         x (list or np.array): Input list or array.
+    
     Returns:
-        result (np.array): The softmax of the array.
+        np.array: The softmax of the array.
+    
     Examples:
         >>> scores = [12, 5, 1]
         >>> softmax(scores)
         array([9.99072278e-01, 9.11035992e-04, 1.66862063e-05])
         >>> sum(softmax(scores))
         1.0
-
     """
     return np.exp(x) / np.sum(np.exp(x), axis=0)
 
 
 def tanh(x):
     """Hyperbolic tangent. Output between -1,1
+    
     Args:
         x (list or np.array): Input list or array.
+    
     Returns:
-        result (np.array): The tanh of the array.
+        np.array: The tanh of the array.
+    
     Examples:
         >>> x = np.array([[1,1,-3],[0.5,-1,1]])
         >>> tanh(x)
         array([[ 0.76159416,  0.76159416, -0.99505475],
                [ 0.46211716, -0.76159416,  0.76159416]])
-
     """
     return np.tanh(x)
 
 
 def sigmoid(x):
     """Sigmoid function. Output between 0,1
+    
     Args:
         x (list or np.array): Input list or array.
+    
     Returns:
-        result (np.array): The sigmoid of the array.
+        np.array: The sigmoid of the array.
+    
     Examples:
         >>> x = np.array([[1,1,-3],[0.5,-1,1]])
         >>> sigmoid(x)
         array([[0.73105858, 0.73105858, 0.04742587],
                [0.62245933, 0.26894142, 0.73105858]])
-
     """
-    return 1. / (1 + np.exp(-x))
+    return 1.0 / (1 + np.exp(-x))
 
 
 def RELU(x, inplace=False):
     """Rectified Linear Unit, removing the negative parts of the input: max(x,0)
     Benchmark: https://stackoverflow.com/a/40013151/5620182
+    
     Args:
         x (list or np.array): Input list or array.
         inplace (bool): Inplace flag.
+    
     Returns:
-        result (np.array): The RELU of the array (if inplace is True).
+        np.array: The RELU of the array (if inplace is True).
+    
     Examples:
         >>> x = np.array([[1,1,-3],[0.5,-1,1]])
         >>> RELU(x)
@@ -75,7 +84,6 @@ def RELU(x, inplace=False):
         >>> x
         array([[1. , 1. , 0. ],
                [0.5, 0. , 1. ]])
-
     """
     if inplace:
         np.maximum(x, 0, x)
@@ -86,11 +94,14 @@ def RELU(x, inplace=False):
 def LeakyRELU(x, alpha=0.3):
     """Leaky Rectified Linear Unit. It allows a small gradient when the unit is not active.
     `f(x) = alpha * x for x < 0, f(x) = x for x >= 0`
+    
     Args:
         x (list or np.array): Input list or array.
         alpha (float): Scale factor.
+    
     Returns:
-        result (np.array): The LeakyRELU of the array .
+        np.array: The LeakyRELU of the array .
+    
     Examples:
         >>> x = np.array([[1,1,-3],[0.5,-1,1]])
         >>> LeakyRELU(x)
@@ -98,7 +109,7 @@ def LeakyRELU(x, alpha=0.3):
                [ 0.5, -0.3,  1. ]])
 
     """
-    return (x >= 0)*x + (x < 0)*alpha*x
+    return (x >= 0) * x + (x < 0) * alpha * x
 
 
 def ELU(x, alpha=1.0):
@@ -106,36 +117,40 @@ def ELU(x, alpha=1.0):
     `f(x) =  alpha * (exp(x) - 1.) for x < 0`,
     `f(x) = x for x >= 0`.
     paper: http://arxiv.org/abs/1511.07289
+    
     Args:
         x (list or np.array): Input list or array.
         alpha (float): Scale factor.
+    
     Returns:
-        result (np.array): The ELU of the array .
+        np.array: The ELU of the array .
+    
     Examples:
         >>> x = np.array([[1,1,-3],[0.5,-1,1]])
         >>> ELU(x)
         array([[ 1.        ,  1.        , -0.95021293],
                [ 0.5       , -0.63212056,  1.        ]])
-
     """
-    return (x >= 0)*x + (x < 0)*(alpha * np.exp(x) - alpha)
+    return (x >= 0) * x + (x < 0) * (alpha * np.exp(x) - alpha)
 
 
 def SELU(x):
     """Scaled Exponential Linear Unit
     paper: https://arxiv.org/abs/1706.02515
+    
     Args:
         x (list or np.array): Input list or array.
         alpha (float): Scale factor.
+    
     Returns:
-        result (np.array): The SELU of the array .
+        np.array: The SELU of the array .
+    
     Examples:
         >>> x = np.array([[1,1,-3],[0.5,-1,1]])
         >>> SELU(x)
         array([[ 1.05070099,  1.05070099, -1.67056873],
                [ 0.52535049, -1.11133074,  1.05070099]])
-
     """
     alpha = 1.6732632423543772848170429916717
     scale = 1.0507009873554804934193349852946
-    return scale * ((x >= 0)*x + (x < 0) * (alpha * np.exp(x) - alpha))
+    return scale * ((x >= 0) * x + (x < 0) * (alpha * np.exp(x) - alpha))
--- python/machine_learning/dataset_split.py
+++ python/machine_learning/dataset_split.py
@@ -4,19 +4,21 @@ from sklearn.utils import indexable
 
 def split_train_test(X, y, test_size=0.2):
     """Split a dataset into train and test sets.
+    
     Args:
         X (np.array or pd.DataFrame): Features.
         y (np.array or pd.DataFrame): Labels.
         test_size (float): Percentage in the test set.
+    
     Returns:
-        X_train, X_test, y_train, y_test (list): List with the dataset splitted.
-    Example:
+        list: List with the dataset splitted (X_train, X_test, y_train, y_test).
+    
+    Examples:
         >>> X = np.random.randint(0,10, (100,5))
         >>> y = np.random.randint(0,1, 100)
         >>> X_train, X_test, y_train, y_test = split_train_test(X, y, test_size=0.2)
         >>> print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
         (80, 5) (20, 5) (80,) (20,)
-
     """
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=test_size, random_state=42, stratify=y
@@ -26,17 +28,20 @@ def split_train_test(X, y, test_size=0.2):
 
 def split_train_val_test(*arrays, val_size=0.2, test_size=0.2, random_seed=42):
     """Split a dataset into train, validation and test sets.
+    
     Args:
         *arrays (list, np.array, pd.DataFrame): Dataset to split, it can be one or two objects with the same number of
                                                 rows.
         val_size (float): Percentage in the validation set.
         test_size (float): Percentage in the test set.
         random_seed (float): Seed.
+    
     Returns:
-        *arrays_out (list): List with the dataset splitted. If the input is one element `X`, the output will be
-                            `X_train, X_val, X_test`. If the input is two elements `X,y`, the output will be
-                            `X_train, X_val, X_test, y_train, y_val, y_test`.
-    Example:
+        list: List with the dataset splitted. If the input is one element `X`, the output will be
+            `X_train, X_val, X_test`. If the input is two elements `X,y`, the output will be
+            `X_train, X_val, X_test, y_train, y_val, y_test`.
+    
+    Examples:
         >>> X = np.random.randint(0,10, (100,5))
         >>> y = np.random.randint(0,1, 100)
         >>> X_train, X_val, X_test, y_train, y_val, y_test = split_train_val_test(X, y, val_size=0.2, test_size=0.2)
@@ -48,7 +53,6 @@ def split_train_val_test(*arrays, val_size=0.2, test_size=0.2, random_seed=42):
         >>> df_train, df_val, df_test = split_train_val_test(df, val_size=0.2, test_size=0.2)
         >>> print(df_train.shape, df_val.shape, df_test.shape)
         (60, 5) (20, 5) (20, 5)
-
     """
     n_arrays = len(arrays)
     if n_arrays == 1:
