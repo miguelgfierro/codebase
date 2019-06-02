c270cedd485ca848771e53167d1f92cfb50f29a7
miguelgfierro <miguelgfierro@users.noreply.github.com>
2019-06-02 08:18:06 +0100

    remove submodule code

--- python/test/pytest_fixtures_in_common_file.py
+++
@@ -1,8 +0,0 @@
-# This file use the common fixtures defined in the file conftest.py.
-# There is no need to import this file, pytest manage this by default.
-
-
-def test_create_nn(nn5):
-    assert nn5.name == "NN"
-    assert nn5.n_layers == 5
-    assert nn5.model is None
