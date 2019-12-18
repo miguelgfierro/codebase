358f21f093ce129a712f393696f4fee25e22585d
miguelgfierro <miguelgfierro@users.noreply.github.com>
2019-12-19 00:11:24 +0100

    sql

--- sql/sql_server/manage_table.sql
+++ sql/sql_server/manage_table.sql
@@ -1,14 +1,20 @@
 -- Drop the table if it exists
 IF OBJECT_ID('table_name') IS NOT NULL
-  DROP TABLE table_name
+DROP TABLE table_name;
 
 -- Create a table with different variables. Not null variables must be always be filled, the rest
 -- may have null values
 CREATE TABLE table_name
 (
-     patient_id varchar(50) not null,
-     size_row integer not null,
-     size_col integer not null,
+     patient_id varchar(50) NOT NULL PRIMARY KEY,
+     size_row INT NOT NULL,
+     size_col INT DEFAULT 0,
      array varbinary(max)
-)
+);
+
+-- Add a new column to the table
+ALTER TABLE t ADD column;
+
+-- Drop column c from the table 
+ALTER TABLE t DROP COLUMN c ;
 
