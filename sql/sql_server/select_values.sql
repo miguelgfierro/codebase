b59f0eeae9a8f765b4a28ecc19c3a72ddff93184
miguelgfierro <miguelgfierro@users.noreply.github.com>
2019-12-18 23:55:48 +0100

    sql

--- sql/sql_server/select_values.sql
+++ sql/sql_server/select_values.sql
@@ -1,10 +1,11 @@
 -- Select all entries equal to a value in a column
-SELECT * FROM table_name WHERE column_name = value
+SELECT * FROM table_name WHERE column_name = value;
 
+-- Select data in columns c1, c2 from a table
+SELECT c1, c2 FROM t;
 
 -- Select top N values of specific column names
 SELECT TOP (5) column_name1, column_name2 FROM table_name;
-
 
 -- Select the last entry equal to a value in a column ordered by other column 
 SELECT TOP(1) * FROM table_name
