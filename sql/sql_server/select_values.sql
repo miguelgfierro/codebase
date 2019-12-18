3a062466b7139881cb5687d6120c980fc45526ae
miguelgfierro <miguelgfierro@users.noreply.github.com>
2019-12-18 23:57:50 +0100

    sql

--- sql/sql_server/select_values.sql
+++ sql/sql_server/select_values.sql
@@ -1,8 +1,11 @@
 -- Select all entries equal to a value in a column
 SELECT * FROM table_name WHERE column_name = value;
 
--- Select data in columns c1, c2 from a table
-SELECT c1, c2 FROM t;
+-- Select data in columns column_name1, column_name2 from a table
+SELECT column_name1, column_name2 FROM table_name;
+
+-- Select distinct rows from a table
+SELECT DISTINCT column_name FROM table_name WHERE column_name = value;
 
 -- Select top N values of specific column names
 SELECT TOP (5) column_name1, column_name2 FROM table_name;
