fa6bfa6a749d49f1ed422a67952d78c635a6647d
miguelgfierro <miguelgfierro@users.noreply.github.com>
2019-12-19 00:03:12 +0100

    sl

--- sql/sql_server/select_values.sql
+++ sql/sql_server/select_values.sql
@@ -17,3 +17,19 @@ WHERE column_name = value ORDER BY time_stamp DESC;
 -- Skip offset of rows and return the next n rows
 SELECT column_name1, column_name2 FROM table_name 
 ORDER BY column_name1 LIMIT n OFFSET offset;
+
+-- Query rows using pattern matching %, _
+SELECT column_name1, column_name2 FROM t1
+WHERE column_name1 [NOT] LIKE pattern;
+
+-- Query rows in a list
+SELECT column_name1, column_name2 FROM table_name
+WHERE column_name1 [NOT] IN value_list;
+
+-- Query rows between two values
+SELECT column_name1, column_name2 FROM table_name
+WHERE column_name1 BETWEEN low AND high;
+
+-- Check if values in a table is NULL or not
+SELECT column_name1, column_name2 FROM table_name
+WHERE column_name1 IS [NOT] NULL;
