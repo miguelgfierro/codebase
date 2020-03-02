0d846bf24d76e606331fd73b4428a1add72cf078
miguelgfierro <miguelgfierro@users.noreply.github.com>
2020-03-02 20:28:16 +0000

    mobile

---
+++ js/system/system_info.js
@@ -0,0 +1,6 @@
+/**
+ * Check whether or not the client is a phone
+ */
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
