8b629464d3b9cf89134090d7eb68c74150329362
Miguel González-Fierro <3491412+miguelgfierro@users.noreply.github.com>
2021-06-02 20:23:19 +0100

    Update paths.sh

--- command_line/linux/paths.sh
+++ command_line/linux/paths.sh
@@ -37,7 +37,7 @@ find . -type f -name '*.db' -delete
 find . -newermt 'Oct 3 00:00' ! -newermt 'Oct 4 00:00' -delete
 
 
-# Remove all files and folders except those with a pattern
+# Remove all files and folders except those with a pattern "*.yaml"
 find my_folder ! -name "*.yaml" 
 find my_folder ! -name "*.yaml" -delete 2>/dev/null  # Ignore warning outputs
 
