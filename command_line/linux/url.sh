68b14f3f54502a9aee939e0e9bb5c5da8e0a6d44
Miguel González-Fierro <3491412+miguelgfierro@users.noreply.github.com>
2022-04-09 20:59:52 +0100

    Create url.sh

---
+++ command_line/linux/url.sh
@@ -0,0 +1,5 @@
+# Download a file in a folder
+wget https:/...csv -P foldername
+
+# Download  a file, resume partially downloaded file (-c), do not download if no change in server (-N)
+wget -c -N https:/...csv
