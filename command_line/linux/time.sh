b2810723c81ae7ec0d5807acc5fa06d9d0537afd
miguelgfierro <miguelgfierro@users.noreply.github.com>
2022-04-11 17:20:28 +0200

    time

---
+++ command_line/linux/time.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Time now
+now=$(date +"%T")
+echo "Current time: $now" # 17:19:32
+
+# Date and time now
+now=$(date +"%d/%m/%Y-%H:%M:%S")
+echo "Current time: $now" # 11/04/2022-17:19:32
