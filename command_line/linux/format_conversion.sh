fee7b841743102355e2025430cec73aad1ed927e
Miguel González-Fierro <3491412+miguelgfierro@users.noreply.github.com>
2022-06-04 20:02:47 +0200

    Update format_conversion.sh

--- command_line/linux/format_conversion.sh
+++ command_line/linux/format_conversion.sh
@@ -5,3 +5,6 @@ inkscape --without-gui --file=foo.svg --export-pdf=foo.pdf
 
 # Convert from PDF to SVG
 inkscape --without-gui --file=foo.pdf  --export-plain-svg=foo.svg
+
+# Convert from mp4 to mp3
+ffmpeg -i 01.mp4 01.mp3
