d779ef3f5a6914f87785796d03c9cf63d18eb0de
miguelgfierro <miguelgfierro@users.noreply.github.com>
2022-07-08 18:43:21 +0200

    wip

--- js/web/popup_after_delay_sendy/code.js
+++ js/web/popup_after_delay_sendy/code.js
@@ -1,1 +1,31 @@
 // Attribution: https://www.sitepoint.com/show-modal-popup-after-time-delay/
+
+function onPopupOpen() {
+    $("#modal-content").show();
+    $("#email").focus();
+}
+
+function onPopupClose() {
+    $("#modal-content").hide();
+    lastFocus.focus();
+}
+
+function displayPopup() {
+    $.colorbox({
+        inline: true,
+        href: "#modal-content",
+        className: "cta",
+        width: 600,
+        height: 350,
+        onComplete: onPopupOpen,
+        onClosed: onPopupClose
+    });
+}
+
+var lastFocus;
+var delay = 3000;
+
+setTimeout(function () {
+    lastFocus = document.activeElement;
+    displayPopup();
+}, delay);
