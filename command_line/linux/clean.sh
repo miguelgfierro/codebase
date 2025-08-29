897d914978e5265fcf4c37eb101977f62c0e62ed
Miguel Fierro <3491412+miguelgfierro@users.noreply.github.com>
2025-08-29 18:06:49 +0200

    Add clean.sh script for system cleanup tasks

---
+++ command_line/linux/clean.sh
@@ -0,0 +1,19 @@
+# Check size
+du -sh /home
+du -sh /
+
+# Clean unused libraries in conda
+conda clean -a
+
+# Review conda environments
+conda env list
+conda env remove -n ENV_NAME
+
+# Clean tmp files
+rm -rf tmp/*
+
+# Clean unused Ubuntu files
+sudo apt-get clean
+sudo apt-get autoremove
+sudo apt-get autoclean
+
